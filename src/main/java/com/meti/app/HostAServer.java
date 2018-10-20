c4e141c9f1055d31dba1a1d0f30fe5f1f47862d1
SirMathhman <minecraftmathhman@gmail.com>
2018-10-20 16:21:04 -0400

    Delete stuff because I have no idea what's going on

--- src/main/java/com/meti/app/HostAServer.java
+++
@@ -1,9 +0,0 @@
-package com.meti.app;
-
-/**
- * @author SirMathhman
- * @version 0.0.0
- * @since 10/6/2018
- */
-public class HostAServer {
-}
--- src/main/java/com/meti/net/Server.java
+++
@@ -1,9 +0,0 @@
-package com.meti.net;
-
-/**
- * @author SirMathhman
- * @version 0.0.0
- * @since 10/6/2018
- */
-public class Server {
-}
