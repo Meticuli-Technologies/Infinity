20c2508cbf5a3e493a87fccf134693d662a24171
SirMathhman <minecraftmathhman@gmail.com>
2018-11-10 14:26:09 -0500

    Fill skeleton for Menu

--- src/main/java/com/meti/app/Menu.java
+++ src/main/java/com/meti/app/Menu.java
@@ -1,9 +1,25 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 11/10/2018
  */
 public class Menu {
+    @FXML
+    public void connectToAServer(){
+
+    }
+
+    @FXML
+    public void hostALocalServer(){
+
+    }
+
+    @FXML
+    public void hostAServer(){
+
+    }
 }
