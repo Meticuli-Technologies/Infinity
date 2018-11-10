0415623ea048ecf09b59fb4802d007074550c7cf
SirMathhman <minecraftmathhman@gmail.com>
2018-11-10 16:58:30 -0500

    Implement partly ServerDisplay.postInitialize()

--- src/main/java/com/meti/app/ServerDisplay.java
+++ src/main/java/com/meti/app/ServerDisplay.java
@@ -1,23 +1,29 @@
 package com.meti.app;
 
+import com.meti.lib.fx.Controller;
 import com.meti.lib.fx.PostInitializable;
+import com.meti.lib.net.Server;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+import java.util.function.Supplier;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 11/10/2018
  */
-public class ServerDisplay implements PostInitializable {
+public class ServerDisplay extends Controller implements PostInitializable {
     @FXML
     private TextField inputField;
 
     @FXML
     private TextArea outputArea;
+
+    private Server server;
 
     @FXML
     public void checkEnter(KeyEvent event){
@@ -32,6 +38,10 @@ public class ServerDisplay implements PostInitializable {
 
     @Override
     public void postInitialize() {
-
+        try {
+            this.server = state.firstOfType(Server.class).orElseThrow((Supplier<Throwable>) () -> new IllegalStateException("Cannot find server to load in display"));
+        } catch (Throwable throwable) {
+            getLogger().error("", throwable);
+        }
     }
 }
