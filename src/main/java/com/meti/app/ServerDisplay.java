5ad06956bb43104d3d8a7771cbae84a0aa0a75b6
SirMathhman <minecraftmathhman@gmail.com>
2018-11-10 15:27:52 -0500

    Create ServerDisplay.fxml and skeleton

---
+++ src/main/java/com/meti/app/ServerDisplay.java
@@ -0,0 +1,23 @@
+package com.meti.app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 11/10/2018
+ */
+public class ServerDisplay {
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private TextArea outputArea;
+
+    @FXML
+    public void checkEnter(){
+
+    }
+}
