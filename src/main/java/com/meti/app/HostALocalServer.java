f9f0d87a5e8bbde19d13790ff8b05e3cdd772c26
SirMathhman <minecraftmathhman@gmail.com>
2018-11-10 14:32:51 -0500

    Add and skeleton HostALocalServer

---
+++ src/main/java/com/meti/app/HostALocalServer.java
@@ -0,0 +1,24 @@
+package com.meti.app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 11/10/2018
+ */
+public class HostALocalServer {
+    @FXML
+    private TextField portField;
+
+    @FXML
+    public void back(){
+
+    }
+
+    @FXML
+    public void next(){
+
+    }
+}
