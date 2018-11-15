d91071fa0213810139475a3d8a3ef64239b6a566
SirMathhman <minecraftmathhman@gmail.com>
2018-11-15 12:40:53 -0500

    Add usage for Server.stop(), handles return value

--- src/main/java/com/meti/app/Main.java
+++ src/main/java/com/meti/app/Main.java
@@ -2,11 +2,14 @@ package com.meti.app;
 
 import com.meti.lib.fx.ControllerLoader;
 import com.meti.lib.fx.ControllerState;
+import com.meti.lib.net.client.Client;
+import com.meti.lib.net.connect.SocketConnection;
 import com.meti.lib.net.server.Server;
 import com.meti.lib.util.Finalizable;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
@@ -16,6 +19,7 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * @author SirMathhman
@@ -27,22 +31,29 @@ public class Main extends Application {
 
     private ControllerState state;
     private Properties properties;
+    private Logger logger;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        properties = loadProperties();
-        state = new ControllerState(
-                primaryStage,
-                LoggerFactory.getLogger(Main.class),
-                properties
-        );
+    public void start(Stage primaryStage) {
+        logger = LoggerFactory.getLogger(Main.class);
 
-        primaryStage.setScene(new Scene(ControllerLoader.load(getClass().getResource("/com/meti/app/Menu.fxml"), state)));
-        primaryStage.show();
+        try {
+            properties = loadProperties();
+            state = new ControllerState(
+                    primaryStage,
+                    logger,
+                    properties
+            );
+
+            primaryStage.setScene(new Scene(ControllerLoader.load(getClass().getResource("/com/meti/app/Menu.fxml"), state)));
+            primaryStage.show();
+        } catch (IOException e) {
+            logger.error("Failed to start application", e);
+        }
     }
 
     private Properties loadProperties() throws IOException {
@@ -66,6 +77,7 @@ public class Main extends Application {
     private void stopServer() throws Exception {
         Optional<Server> serverOptional = state.firstOfType(Server.class);
         if (serverOptional.isPresent()) {
+            Optional<Set<Client<SocketConnection>>> clientSetOptional;
             if (properties.containsKey("stop_duration")) {
                 long milis;
                 try {
@@ -73,9 +85,24 @@ public class Main extends Application {
                 } catch (NumberFormatException e) {
                     milis = 1000;
                 }
-                serverOptional.get().stop(Duration.ofMillis(milis));
+                clientSetOptional = serverOptional.get().stop(Duration.ofMillis(milis));
             } else {
-                serverOptional.get().stop();
+                clientSetOptional = serverOptional.get().stop();
+            }
+
+            if (clientSetOptional.isPresent()) {
+                Set<Client<SocketConnection>> clientSet = clientSetOptional.get();
+                StringBuilder builder = new StringBuilder();
+                builder.append("Located ")
+                        .append(clientSet.size())
+                        .append(" clients that did not stop cleanly: ");
+
+                clientSet.forEach(socketConnectionClient -> builder.append("\n\t")
+                        .append(socketConnectionClient.connection.socket.getInetAddress()));
+
+                logger.error(builder.toString());
+            } else {
+                logger.info("All clients stopped cleanly");
             }
         }
     }
