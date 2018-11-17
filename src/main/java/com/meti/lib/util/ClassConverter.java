08ceb192334ff290a35dfcefaf0c10316dea957e
SirMathhman <minecraftmathhman@gmail.com>
2018-11-17 17:51:53 -0500

    Introduce ClassConverter
    Serves as a common case for conversion

---
+++ src/main/java/com/meti/lib/util/ClassConverter.java
@@ -0,0 +1,24 @@
+package com.meti.lib.util;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 11/17/2018
+ */
+public class ClassConverter<T> implements Converter<T> {
+    public final Class<T> tClass;
+
+    public ClassConverter(Class<T> tClass) {
+        this.tClass = tClass;
+    }
+
+    @Override
+    public T apply(Object o) {
+        return tClass.cast(o);
+    }
+
+    @Override
+    public boolean test(Object o) {
+        return tClass.isAssignableFrom(o.getClass());
+    }
+}
