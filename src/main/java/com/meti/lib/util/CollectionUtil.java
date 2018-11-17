847893b5cc85a09f0e2c980ad2275f16608de96e
SirMathhman <minecraftmathhman@gmail.com>
2018-11-17 17:46:53 -0500

    Create convert method in CollectionUtil
    Used for reducing vague instances into generic instances

--- src/main/java/com/meti/lib/util/CollectionUtil.java
+++ src/main/java/com/meti/lib/util/CollectionUtil.java
@@ -2,6 +2,8 @@ package com.meti.lib.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author SirMathhman
@@ -11,7 +13,12 @@ import java.util.Arrays;
 public class CollectionUtil {
     private CollectionUtil(){}
 
+    @SafeVarargs
     public static <T> ArrayList<T> asArrayList(T... ts){
         return new ArrayList<>(Arrays.asList(ts));
+    }
+
+    public static <T> List<T> convert(Collection<?> collection){
+        return null;
     }
 }
