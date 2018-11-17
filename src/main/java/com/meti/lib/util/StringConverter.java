690db79accbed71e97dda2961b14b138879e2d03
SirMathhman <minecraftmathhman@gmail.com>
2018-11-17 17:52:27 -0500

    Introduce StringConverter
    Another common and more specific case

---
+++ src/main/java/com/meti/lib/util/StringConverter.java
@@ -0,0 +1,12 @@
+package com.meti.lib.util;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 11/17/2018
+ */
+public class StringConverter extends ClassConverter<String> {
+    public StringConverter() {
+        super(String.class);
+    }
+}
