2b5071b14fe0295a34f4fbd0ac226ebf0fcd8b41
SirMathhman <minecraftmathhman@gmail.com>
2018-11-18 12:23:11 -0500

    Create Chat class

---
+++ src/main/java/com/meti/app/Chat.java
@@ -0,0 +1,18 @@
+package com.meti.app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 11/18/2018
+ */
+public class Chat {
+    @FXML
+    private TextField input;
+
+    @FXML
+    private TextArea output;
+}
