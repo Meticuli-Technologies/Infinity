f4baf4ef5347dadd477cabdde72537fbd41f0e08
SirMathhman <minecraftmathhman@gmail.com>
2018-12-19 19:26:02 -0500

    Add Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 12/19/2018
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(new AnchorPane()));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
