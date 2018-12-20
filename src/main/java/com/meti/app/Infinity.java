a1eeb61f634b118a49d11d8b53771c20a91a152b
SirMathhman <minecraftmathhman@gmail.com>
2018-12-19 19:31:52 -0500

    Attach Display.fxml to Infinity

--- src/main/java/com/meti/app/Infinity.java
+++ src/main/java/com/meti/app/Infinity.java
@@ -1,16 +1,24 @@
 package com.meti.app;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Infinity {
     public Infinity() {
     }
 
     public void start(Stage primaryStage) {
-        primaryStage.setScene(new Scene(new AnchorPane()));
-        primaryStage.show();
+        try {
+            primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/meti/app/Display.fxml"))));
+            primaryStage.show();
+        } catch (IOException e) {
+
+            //TODO: handle exceptions
+            e.printStackTrace();
+        }
     }
 
     public void stop() {
