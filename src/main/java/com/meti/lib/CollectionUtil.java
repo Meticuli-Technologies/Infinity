24388e9cb1ebd4f1337b90b598e2a815c00e6418
SirMathhman <sirmathhman@gmail.com>
2019-01-23 15:42:13 -0500

    Add search methods in State

--- src/main/java/com/meti/lib/CollectionUtil.java
+++ src/main/java/com/meti/lib/CollectionUtil.java
@@ -8,6 +8,12 @@ public final class CollectionUtil {
     }
 
     public static <T> T toSingle(Collection<T> toSingle) {
+        if (toSingle.isEmpty()) {
+            throw new IllegalArgumentException("No elements found in " + toSingle);
+        }
+        if (toSingle.size() > 1) {
+            throw new IllegalArgumentException("Too many elements found in " + toSingle);
+        }
         return new ArrayList<>(toSingle).get(0);
     }
 }
--- src/main/java/com/meti/lib/State.java
+++ src/main/java/com/meti/lib/State.java
@@ -1,5 +1,23 @@
 package com.meti.lib;
 
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class State {
     private final BucketManager<Object> manager = new BucketManager<>(o -> new Bucket<>(new TypePredicate<>(o.getClass()), CollectionConsumer.empty()));
+
+    public <T> T storedElement(Class<T> tClass) {
+        return CollectionUtil.toSingle(storedSet(tClass));
+    }
+
+    public <T> Set<T> storedSet(Class<T> tClass) {
+        return manager.search(tClass)
+                .stream()
+                .flatMap((Function<Bucket<Object>, Stream<?>>) objectBucket -> objectBucket.getElements().stream())
+                .filter(o -> tClass.isAssignableFrom(o.getClass()))
+                .map(tClass::cast)
+                .collect(Collectors.toSet());
+    }
 }
