3b50566eaaad3ffc1dbd1c83b6aa933a04cdbe60
SirMathhman <sirmathhman@gmail.com>
2019-01-23 15:45:21 -0500

    Add an initial constructor to State

--- src/main/java/com/meti/lib/State.java
+++ src/main/java/com/meti/lib/State.java
@@ -5,6 +5,7 @@ import com.meti.lib.bucket.BucketManager;
 import com.meti.lib.bucket.CollectionConsumer;
 import com.meti.lib.bucket.TypePredicate;
 
+import java.util.Arrays;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -12,6 +13,10 @@ import java.util.stream.Stream;
 
 public class State {
     private final BucketManager<Object> manager = new BucketManager<>(o -> new Bucket<>(new TypePredicate<>(o.getClass()), CollectionConsumer.empty()));
+
+    public State(Object... initial) {
+        Arrays.stream(initial).forEach(manager::add);
+    }
 
     public <T> T storedElement(Class<T> tClass) {
         return CollectionUtil.toSingle(storedSet(tClass));
