9c3e00fd07db004b882ad156e33e815c52e6272f
SirMathhman <sirmathhman@gmail.com>
2019-01-23 16:25:39 -0500

    Skeleton NewConnection.fxml

--- src/main/java/com/meti/app/NewConnection.java
+++ src/main/java/com/meti/app/NewConnection.java
@@ -1,4 +1,13 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
 public class NewConnection {
+    @FXML
+    private ListView<String> typeView;
+
+    @FXML
+    private AnchorPane contentPane;
 }
