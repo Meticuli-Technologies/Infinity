f0ff66027a8656a22da87b7e666cdecb117e9c4a
SirMathhman <sirmathhman@gmail.com>
2019-01-23 16:20:10 -0500

    Add NewConnection

--- src/main/java/com/meti/app/Display.java
+++ src/main/java/com/meti/app/Display.java
@@ -1,11 +1,32 @@
 package com.meti.app;
 
+import com.meti.lib.fx.Controller;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
-public class Display {
+import java.io.IOException;
+import java.net.URL;
+
+public class Display extends Controller {
+    @FXML
+    private AnchorPane contentPane;
+
     @FXML
     public void exit() {
         Platform.exit();
+    }
+
+    @FXML
+    public void openNewConnection() {
+        try {
+            load(newConnectionFXML());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public URL newConnectionFXML() {
+        return getClass().getResource("/com/meti/app/NewConnection.fxml");
     }
 }
