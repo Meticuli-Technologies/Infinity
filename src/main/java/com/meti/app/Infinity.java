1f5a098f759f9163bf56e3051131eb6f9c35b9f0
SirMathhman <minecraftmathhman@gmail.com>
2019-01-27 16:22:58 -0500

    Add helper methods to State

--- src/main/java/com/meti/app/Infinity.java
+++ src/main/java/com/meti/app/Infinity.java
@@ -11,7 +11,7 @@ public class Infinity {
     private final InfinityState state = new InfinityState();
 
     public void start(Stage primaryStage) {
-
+        state.buckets.add(primaryStage);
     }
 
     public void stop() {
--- src/main/java/com/meti/lib/state/State.java
+++ src/main/java/com/meti/lib/state/State.java
@@ -1,12 +1,13 @@
 package com.meti.lib.state;
 
-import com.meti.lib.bucket.Bucket;
-import com.meti.lib.bucket.BucketManager;
-import com.meti.lib.bucket.BufferedConsumer;
-import com.meti.lib.bucket.TypePredicate;
+import com.meti.lib.bucket.*;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author SirMathhman
@@ -18,6 +19,26 @@ public class State {
 
     public State(Object... objects){
         buckets.addAll(Arrays.asList(objects));
+    }
+
+    public <T> T singleContent(Class<T> typeClass) {
+        Optional<Contentable<?, ?>> content = buckets.searchForSingle(typeClass).getContent();
+        if (content.isPresent()) {
+            return typeClass.cast(content.get().toSingle());
+        } else {
+            throw new IllegalStateException(buckets + " does not contain instances of " + Contentable.class);
+        }
+    }
+
+    public <T> List<T> multipleContent(Class<T> typeClass) {
+        return buckets.search(typeClass)
+                .stream()
+                .map(Bucket::getContent)
+                .flatMap((Function<Optional<Contentable<?, ?>>, Stream<Contentable<?, ?>>>) Optional::stream)
+                .flatMap((Function<Contentable<?, ?>, Stream<?>>) contentable -> contentable.getContent().stream())
+                .filter(o -> typeClass.isAssignableFrom(o.getClass()))
+                .map(o -> typeClass.cast(o.getClass()))
+                .collect(Collectors.toList());
     }
 
     private static class BucketAllocator implements Function<Object, Bucket<Object>> {
