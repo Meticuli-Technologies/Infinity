7b5f0eefff6053bfc88aefb9dd1fa3a15c0cda63
SirMathhman <minecraftmathhman@gmail.com>
2019-01-27 15:47:48 -0500

    Add skeleton

--- src/main/java/com/meti/app/Menu.java
+++ src/main/java/com/meti/app/Menu.java
@@ -1,9 +1,34 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/27/2019
  */
 public class Menu {
+    @FXML
+    private Text versionText;
+
+    @FXML
+    public void findAConnection() {
+
+    }
+
+    @FXML
+    public void hostAServer() {
+
+    }
+
+    @FXML
+    public void useLocally() {
+
+    }
+
+    @FXML
+    public void openSettings() {
+
+    }
 }
