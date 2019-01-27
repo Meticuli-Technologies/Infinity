586987d12a5b76868a2fffb406635db3d6b63cf7
SirMathhman <minecraftmathhman@gmail.com>
2019-01-27 15:50:46 -0500

    Add Bucket and BucketManager - Implement Bucket

---
+++ src/main/java/com/meti/lib/bucket/Bucket.java
@@ -0,0 +1,27 @@
+package com.meti.lib.bucket;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/27/2019
+ */
+public class Bucket<T> {
+    private final Predicate<T> filter;
+    private final Consumer<T> handler;
+
+    public Bucket(Predicate<T> filter, Consumer<T> handler) {
+        this.filter = filter;
+        this.handler = handler;
+    }
+
+    public boolean process(T t) {
+        if (filter.test(t)) {
+            handler.accept(t);
+            return true;
+        }
+        return false;
+    }
+}
