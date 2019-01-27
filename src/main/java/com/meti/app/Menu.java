5963fe3b01ec1bd1d7827882fa365e152a442bc9
SirMathhman <minecraftmathhman@gmail.com>
2019-01-27 15:43:10 -0500

    Clear files

--- src/main/java/com/meti/app/Menu.java
+++
@@ -1,4 +0,0 @@
-package com.meti.app;
-
-public class Menu {
-}
--- src/main/java/com/meti/lib/bucket/Parameterized.java
+++
@@ -1,7 +0,0 @@
-package com.meti.lib.bucket;
-
-import java.util.Set;
-
-public interface Parameterized<T> {
-    Set<T> getParameters();
-}
