6964ad202b6615818fe226997583aafad6ae7318
SirMathhman <minecraftmathhman@gmail.com>
2019-01-27 15:51:36 -0500

    Add Bucket to BucketManager

--- src/main/java/com/meti/lib/bucket/BucketManager.java
+++ src/main/java/com/meti/lib/bucket/BucketManager.java
@@ -1,10 +1,19 @@
 package com.meti.lib.bucket;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/27/2019
  */
 public class BucketManager<T> {
+    private final Set<Bucket<T>> buckets = new HashSet<>();
+    private final Function<T, Bucket<T>> allocator;
 
+    public BucketManager(Function<T, Bucket<T>> allocator) {
+        this.allocator = allocator;
+    }
 }
