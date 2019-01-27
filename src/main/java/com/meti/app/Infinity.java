291b4dcf67c7a4c204286843df4c7475e0730174
SirMathhman <minecraftmathhman@gmail.com>
2019-01-27 15:48:47 -0500

    Implement Main

--- src/main/java/com/meti/app/Infinity.java
+++ src/main/java/com/meti/app/Infinity.java
@@ -1,9 +1,18 @@
 package com.meti.app;
 
+import javafx.stage.Stage;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/27/2019
  */
 public class Infinity {
+    public void start(Stage primaryStage) {
+
+    }
+
+    public void stop() {
+
+    }
 }
--- src/main/java/com/meti/app/Main.java
+++ src/main/java/com/meti/app/Main.java
@@ -1,9 +1,27 @@
 package com.meti.app;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/27/2019
  */
-public class Main {
+public class Main extends Application {
+    private final Infinity infinity = new Infinity();
+
+    @Override
+    public void start(Stage primaryStage) {
+        infinity.start(primaryStage);
+    }
+
+    @Override
+    public void stop() {
+        infinity.stop();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
