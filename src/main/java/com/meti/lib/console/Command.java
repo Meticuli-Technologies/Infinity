ee3e546ca9cf5011753d80964dddf775c98442fb
SirMathhman <minecraftmathhman@gmail.com>
2019-01-27 16:31:12 -0500

    Add Command and Console

---
+++ src/main/java/com/meti/lib/console/Command.java
@@ -0,0 +1,26 @@
+package com.meti.lib.console;
+
+import java.util.Iterator;
+import java.util.StringTokenizer;
+import java.util.function.Function;
+import java.util.stream.StreamSupport;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/27/2019
+ */
+public class Command {
+    private final String[] args;
+
+    public Command(String line) {
+        Iterable<Object> iterable = () -> new StringTokenizer(line).asIterator();
+        this.args = StreamSupport.stream(iterable.spliterator(), false)
+                .map(Object::toString)
+                .toArray(String[]::new);
+    }
+
+    public Command(String[] args) {
+        this.args = args;
+    }
+}
---
+++ src/main/java/com/meti/lib/console/Console.java
@@ -0,0 +1,10 @@
+package com.meti.lib.console;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/27/2019
+ */
+public class Console {
+
+}
