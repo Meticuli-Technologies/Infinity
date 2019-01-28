10ca3a1d4d663fdd8b59a8cc92a82fba91d29b8e
SirMathhman <minecraftmathhman@gmail.com>
2019-01-28 18:06:58 -0500

    Add Clause to clean everything

--- src/main/java/com/meti/lib/fx/ControllerLoader.java
+++ src/main/java/com/meti/lib/fx/ControllerLoader.java
@@ -2,15 +2,16 @@ package com.meti.lib.fx;
 
 import com.meti.lib.clazz.ClassSource;
 import com.meti.lib.state.State;
+import com.meti.lib.util.Clause;
 import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.net.URL;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static com.meti.lib.util.Clause.*;
 
 /**
  * @author SirMathhman
@@ -47,20 +48,10 @@ public class ControllerLoader extends FXMLLoader {
         Set<Class<?>> filtered = filterForWizards(wizardClass);
 
         Set<Wizard> wizards = filtered.stream()
-                .map((Function<Class<?>, Optional<Constructor<?>>>) aClass -> {
-                    try {
-                        return Optional.of(aClass.getDeclaredConstructor(state.getClass()));
-                    } catch (NoSuchMethodException e) {
-                        return Optional.empty();
-                    }
-                }).flatMap(Optional::stream)
-                .map((Function<Constructor<?>, Optional<Object>>) constructor -> {
-                    try {
-                        return Optional.of(constructor.newInstance(state));
-                    } catch (Exception e) {
-                        return Optional.empty();
-                    }
-                }).flatMap(Optional::stream)
+                .map(wrap(aClass -> aClass.getDeclaredConstructor(state.getClass())))
+                .flatMap(Optional::stream)
+                .map(wrap(constructor -> constructor.newInstance(state)))
+                .flatMap(Optional::stream)
                 .filter(o -> Wizard.class.isAssignableFrom(o.getClass()))
                 .map(Wizard.class::cast)
                 .collect(Collectors.toSet());
