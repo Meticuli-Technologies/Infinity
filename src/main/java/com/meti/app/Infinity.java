e257bee4208dfc762cc4373e30d7a8519a51a161
SirMathhman <minecraftmathhman@gmail.com>
2019-01-28 18:26:38 -0500

    Add Properties

--- src/main/java/com/meti/app/Infinity.java
+++ src/main/java/com/meti/app/Infinity.java
@@ -5,7 +5,12 @@ import com.meti.lib.fx.ControllerLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.logging.Level;
 
 /**
@@ -14,25 +19,44 @@ import java.util.logging.Level;
  * @since 1/27/2019
  */
 public class Infinity {
+    private static final Path PROPERTIES_DEFAULT_PATH = Paths.get(".\\Infinity.properties");
+
     //nonfinal variable is OK, because this variable will be injected
     private InfinityState state;
     private Console console;
+    private Properties properties;
 
     public void start(Stage primaryStage) {
         console = new Console(Infinity.class.getSimpleName());
         console.log(Level.INFO, "Starting Infinity");
 
         try {
+            properties = loadProperties();
             state = new InfinityState(
                     primaryStage,
+                    properties,
                     console
             );
 
-            primaryStage.setScene(new Scene(ControllerLoader.load(getMenuURL(), state)));
-            primaryStage.show();
+            loadMenu(primaryStage);
         } catch (Exception e) {
             console.log(Level.SEVERE, "Exception in starting Infinity", e);
         }
+    }
+
+    private Properties loadProperties() throws IOException {
+        Properties properties = new Properties();
+        if (Files.exists(PROPERTIES_DEFAULT_PATH)) {
+            Files.createFile(PROPERTIES_DEFAULT_PATH);
+        } else {
+            properties.load(Files.newInputStream(PROPERTIES_DEFAULT_PATH));
+        }
+        return properties;
+    }
+
+    private void loadMenu(Stage primaryStage) throws java.io.IOException {
+        primaryStage.setScene(new Scene(ControllerLoader.load(getMenuURL(), state)));
+        primaryStage.show();
     }
 
     private URL getMenuURL() {
@@ -41,5 +65,15 @@ public class Infinity {
 
     public void stop() {
         console.log(Level.INFO, "Stopping Infinity");
+
+        try {
+            storeProperties();
+        } catch (IOException e) {
+            console.log(Level.SEVERE, "Exception in stopping Infinity", e);
+        }
+    }
+
+    private void storeProperties() throws IOException {
+        properties.store(Files.newOutputStream(PROPERTIES_DEFAULT_PATH), null);
     }
 }
