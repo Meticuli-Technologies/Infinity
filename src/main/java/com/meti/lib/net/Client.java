b7b03e73a1f5bea9c8f3bcb850c2f6cf061dbd00
SirMathhman <sirmathhman@gmail.com>
2019-01-29 10:44:33 -0500

    Add implementation to Client

--- src/main/java/com/meti/lib/net/Client.java
+++ src/main/java/com/meti/lib/net/Client.java
@@ -1,9 +1,44 @@
 package com.meti.lib.net;
 
-public class Client {
-    private final Input input = new Input();
+import com.meti.lib.util.Loop;
 
-    public void listen(){
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
+public class Client implements Closeable {
+    public final Input input = new Input();
+    private final ObjectConnection connection;
+    private Loop loop = new ClientLoop();
+    private Future<Optional<Exception>> loopSubmission;
+
+    public Client(ObjectConnection connection) {
+        this.connection = connection;
+    }
+
+    public Future<Optional<Exception>> start(ExecutorService service) {
+        return loopSubmission = service.submit(loop);
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (!loopSubmission.isCancelled()) {
+            stop();
+        }
+
+        connection.close();
+    }
+
+    public void stop() {
+        loopSubmission.cancel(true);
+    }
+
+    private class ClientLoop extends Loop {
+        @Override
+        public void loop() throws Exception {
+            input.accept(connection.read());
+        }
     }
 }
