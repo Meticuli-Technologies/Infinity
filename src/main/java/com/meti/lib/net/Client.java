d2e2506b9c9f2881ed715ea4fb330796db47570b
SirMathhman <sirmathhman@gmail.com>
2019-01-29 10:13:59 -0500

    Add Loop

--- src/main/java/com/meti/lib/net/Client.java
+++ src/main/java/com/meti/lib/net/Client.java
@@ -3,4 +3,7 @@ package com.meti.lib.net;
 public class Client {
     private final Input input = new Input();
 
+    public void listen(){
+
+    }
 }
---
+++ src/main/java/com/meti/lib/util/Loop.java
@@ -0,0 +1,31 @@
+package com.meti.lib.util;
+
+import java.util.Optional;
+import java.util.concurrent.Callable;
+
+public abstract class Loop implements Callable<Optional<Exception>> {
+    @Override
+    public Optional<Exception> call() {
+        try {
+            start();
+
+            while (!Thread.interrupted()) {
+                loop();
+            }
+
+            stop();
+
+            return Optional.empty();
+        } catch (Exception e) {
+            return Optional.of(e);
+        }
+    }
+
+    public void start() throws Exception {
+    }
+
+    public abstract void loop() throws Exception;
+
+    public void stop() throws Exception {
+    }
+}
