26c9d4ec94907d957a7417062c79a3813a4f0b8f
SirMathhman <minecraftmathhman@gmail.com>
2019-01-29 13:45:44 -0500

    Implement Server

--- src/main/java/com/meti/lib/net/Server.java
+++ src/main/java/com/meti/lib/net/Server.java
@@ -2,14 +2,21 @@ package com.meti.lib.net;
 
 import com.meti.lib.util.Loop;
 
+import java.util.function.Consumer;
+
 public abstract class Server extends LoopHandler {
+    private final Consumer<Client> clientConsumer;
+
+    protected Server(Consumer<Client> clientConsumer) {
+        this.clientConsumer = clientConsumer;
+    }
 
     @Override
     public Loop getLoop() {
         return new Loop() {
             @Override
             public void loop() throws Exception {
-
+                clientConsumer.accept(accept());
             }
         };
     }
