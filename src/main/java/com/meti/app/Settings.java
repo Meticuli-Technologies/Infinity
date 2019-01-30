8cc041ececee8178e9e6d563064bb32426c7726f
SirMathhman <minecraftmathhman@gmail.com>
2019-01-29 20:25:06 -0500

    Add skeleton to Settings

--- src/main/java/com/meti/app/Settings.java
+++ src/main/java/com/meti/app/Settings.java
@@ -1,9 +1,14 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 1/29/2019
  */
 public class Settings {
+    @FXML
+    private GridPane contentPane;
 }
