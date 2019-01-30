660c2d0cb450c53b688c304f80126e685e48cc70
SirMathhman <minecraftmathhman@gmail.com>
2019-01-30 11:36:40 -0500

    Add InfinityController - provides some convenience methods

---
+++ src/main/java/com/meti/app/InfinityController.java
@@ -0,0 +1,21 @@
+package com.meti.app;
+
+import com.meti.lib.console.Console;
+import com.meti.lib.fx.Controller;
+
+import java.util.Properties;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 1/30/2019
+ */
+public class InfinityController extends Controller {
+    public Console getConsole() {
+        return state.get().singleContent(Console.class);
+    }
+
+    public Properties getProperties() {
+        return state.get().singleContent(Properties.class);
+    }
+}
