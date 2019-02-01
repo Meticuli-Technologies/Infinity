411bd6b1dc24d3c59d7fc4f7071fed504608ea6b
SirMathhman <minecraftmathhman@gmail.com>
2019-02-01 17:10:39 -0500

    For future: fix timeout exception

--- modules/SocketModule/src/com/meti/app/SocketConnectionCreatorView.java
+++ modules/SocketModule/src/com/meti/app/SocketConnectionCreatorView.java
@@ -1,8 +1,8 @@
 package com.meti.app;
 
+import com.meti.app.control.InfinityController;
 import com.meti.lib.SocketConnection;
-import com.meti.lib.fx.Controller;
-import com.meti.lib.util.Counter;
+import com.meti.lib.concurrent.ThreadManager;
 import javafx.animation.AnimationTimer;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -17,13 +17,16 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 2/1/2019
  */
-public class SocketConnectionCreatorView extends Controller implements Initializable {
+public class SocketConnectionCreatorView extends InfinityController implements Initializable {
     @FXML
     TextField addressField;
 
@@ -40,26 +43,40 @@ public class SocketConnectionCreatorView extends Controller implements Initializ
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                if (now % 512 == 0) {
-                    refreshSocket();
+                if (now % 100 == 0) {
+                    refresh();
                 }
             }
         };
         timer.start();
     }
 
-    public void refreshSocket() {
+    public void refresh() {
         try {
-            socketProperty.set(new Socket(InetAddress.getByName(addressField.getText()), Integer.parseInt(portField.getText())));
-            statusText.setText("Nominal");
+            InetAddress address = InetAddress.getByName(addressField.getText());
+            int port = Integer.parseInt(portField.getText());
+
+            Future<String> result = state.get().singleContent(ThreadManager.class)
+                    .submit(() -> socketListener(address, port));
+
+            statusText.setText(result.get(10, TimeUnit.MILLISECONDS));
+        } catch (Exception e) {
+            console.log(Level.WARNING, e);
+        }
+    }
+
+    public String socketListener(InetAddress address, int port) {
+        try {
+            socketProperty.set(new Socket(address, port));
+            return "Nominal";
         } catch (Exception e) {
             socketProperty.set(null);
-            statusText.setText(e.getMessage());
+            return e.getMessage();
         }
     }
 
     public Optional<SocketConnection> getConnection() throws IOException {
-        refreshSocket();
+        refresh();
 
         Socket socket = socketProperty.get();
         if (socket != null) {
