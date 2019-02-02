df820afde81db1cf08461dccfce8077ff461d782
SirMathhman <minecraftmathhman@gmail.com>
2019-02-02 14:55:38 -0500

    Add skeleton

--- src/main/java/com/meti/app/control/ServerManager.java
+++ src/main/java/com/meti/app/control/ServerManager.java
@@ -1,9 +1,43 @@
 package com.meti.app.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 2/2/2019
  */
-public class ServerManager {
+public class ServerManager extends InfinityController {
+    @FXML
+    private AnchorPane dataPane;
+
+    @FXML
+    private ListView<String> serverListView;
+
+    @FXML
+    public void addServer() {
+
+    }
+
+    @FXML
+    public void closeServer() {
+
+    }
+
+    @FXML
+    public void clearServers() {
+
+    }
+
+    @FXML
+    public void doContinue() {
+
+    }
+
+    @FXML
+    public void back() {
+        toBack();
+    }
 }
