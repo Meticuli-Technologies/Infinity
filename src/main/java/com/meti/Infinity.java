4322302b58b938c5096607c129a230a58c578dfe
SirMathhman <minecraftmathhman@gmail.com>
2019-02-13 15:48:24 -0500

    Organize

--- src/main/java/com/meti/Infinity.java
+++ src/main/java/com/meti/Infinity.java
@@ -1,4 +1,4 @@
-package com;
+package com.meti;
 
 import javafx.stage.Stage;
 
--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,4 +1,4 @@
-package com;
+package com.meti;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
