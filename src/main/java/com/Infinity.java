f375017f173d2807887ef185541fda0f02e98a59
SirMathhman <minecraftmathhman@gmail.com>
2019-02-13 15:47:21 -0500

    Add core files

---
+++ src/main/java/com/Infinity.java
@@ -0,0 +1,17 @@
+package com;
+
+import javafx.stage.Stage;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 2/13/2019
+ */
+public class Infinity {
+    public void start(Stage primaryStage) {
+    }
+
+    public void stop() {
+    }
+}
+
