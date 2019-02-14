67f610c7860d16346c0c40b71a7f158f85a6f1f2
SirMathhman <minecraftmathhman@gmail.com>
2019-02-13 20:53:05 -0500

    Add SettingManager

---
+++ src/main/java/com/meti/lib/setting/Setting.java
@@ -0,0 +1,9 @@
+package com.meti.lib.setting;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 2/13/2019
+ */
+public class Setting {
+}
---
+++ src/main/java/com/meti/lib/setting/SettingManager.java
@@ -0,0 +1,15 @@
+package com.meti.lib.setting;
+
+import com.meti.lib.Manager;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 2/13/2019
+ */
+public class SettingManager extends Manager<Setting> {
+    @Override
+    protected Setting create() {
+        return null;
+    }
+}
