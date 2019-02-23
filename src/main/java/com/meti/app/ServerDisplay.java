a13ebe1b32e2751f9fcd2c020ffeb57fd8779367
SirMathhman <minecraftmathhman@gmail.com>
2019-02-23 15:56:33 -0500

    Add skeleton to ServerDisplay

--- src/main/java/com/meti/app/ServerDisplay.java
+++ src/main/java/com/meti/app/ServerDisplay.java
@@ -1,9 +1,31 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 2/23/2019
  */
 public class ServerDisplay {
+    @FXML
+    private BorderPane contentPane;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    public void back(){
+
+    }
+
+    @FXML
+    public void next(){
+
+    }
 }
