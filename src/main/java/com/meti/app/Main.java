e82db3049c09aa4d741d18920757a1a69a318e17
SirMathhman <sirmathhman@gmail.com>
2019-02-26 13:58:57 -0500

    Add Controller system

--- src/main/java/com/meti/app/Main.java
+++ src/main/java/com/meti/app/Main.java
@@ -4,8 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
 
 /**
  * @author SirMathhman
@@ -20,10 +23,43 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/meti/app/Menu.fxml"))));
+            URL resource = getClass().getResource("/com/meti/app/Menu.fxml");
+            FXMLLoader loader = new FXMLLoader(resource);
+            primaryStage.setScene(new Scene(loader.load()));
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private class ControllerLoader extends FXMLLoader {
+        private final State state;
+
+        public ControllerLoader(URL location, State state) {
+            super(location);
+            this.state = state;
+
+            setControllerFactory(param -> {
+                if (!Controller.class.isAssignableFrom(param)) {
+                    throw new IllegalArgumentException(param.getName() + " is not assignable to " + Controller.class);
+                }
+                try {
+                    return param.getConstructor(State.class).newInstance(state);
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("Failed to instantiate " + param.getName());
+                }
+            });
+        }
+
+        private class Controller {
+            protected final State state;
+
+            public Controller(State state) {
+                this.state = state;
+            }
+        }
+    }
+
+    private class State extends HashSet<Object> {
     }
 }
