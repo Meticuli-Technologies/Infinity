2d8eb22b0f7ccbc289e9234611f8dc9132c5ecec
SirMathhman <sirmathhman@gmail.com>
2019-02-26 13:35:49 -0500

    Implement Menu

--- src/main/java/com/meti/app/Menu.java
+++ src/main/java/com/meti/app/Menu.java
@@ -1,6 +1,11 @@
 package com.meti.app;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * @author SirMathhman
@@ -10,11 +15,23 @@ import javafx.fxml.FXML;
 public class Menu {
     @FXML
     public void connect() {
-
+        try {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/meti/app/ClientMenu.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     public void host() {
-
+        try {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/meti/app/ServerMenu.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
