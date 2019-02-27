b2178164fec70ee182b8691d32bce3a768d2c010
SirMathhman <sirmathhman@gmail.com>
2019-02-27 16:13:39 -0500

    Add ControllerLoader to Menu

--- src/main/java/com/meti/app/Menu.java
+++ src/main/java/com/meti/app/Menu.java
@@ -2,8 +2,8 @@ package com.meti.app;
 
 import com.meti.lib.State;
 import com.meti.lib.fx.Controller;
+import com.meti.lib.fx.ControllerLoader;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -21,7 +21,7 @@ public class Menu extends Controller {
     public void connect() {
         try {
             Stage stage = state.byClassToSingle(Stage.class).orElse(new Stage());
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/meti/app/ClientMenu.fxml"))));
+            stage.setScene(new Scene(ControllerLoader.load(getClass().getResource("/com/meti/app/ClientMenu.fxml"))));
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
@@ -32,7 +32,7 @@ public class Menu extends Controller {
     public void host() {
         try {
             Stage stage = state.byClassToSingle(Stage.class).orElse(new Stage());
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/meti/app/ServerMenu.fxml"))));
+            stage.setScene(new Scene(ControllerLoader.load(getClass().getResource("/com/meti/app/ServerMenu.fxml"))));
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
