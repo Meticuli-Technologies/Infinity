3ae00a8994de5d8e9f63d4f05d9458f3bedcaf4b
SirMathhman <minecraftmathhman@gmail.com>
2019-02-28 19:04:16 -0500

    Add skeleton to ClientDisplay

--- src/main/java/com/meti/app/ClientDisplay.java
+++ src/main/java/com/meti/app/ClientDisplay.java
@@ -2,6 +2,10 @@ package com.meti.app;
 
 import com.meti.lib.State;
 import com.meti.lib.fx.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.net.Socket;
 
@@ -11,10 +15,21 @@ import java.net.Socket;
  * @since 2/28/2019
  */
 public class ClientDisplay extends Controller  {
+    @FXML
+    private TextField input;
+
+    @FXML
+    private TextArea output;
+
     public ClientDisplay(State state) {
         super(state);
     }
 
     public void load(Socket socket) {
+    }
+
+    @FXML
+    public void handleInput(KeyEvent event){
+
     }
 }
