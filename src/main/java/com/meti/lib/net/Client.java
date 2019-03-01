72922793f052f74bb1bc55e0435ec9f256d48997
SirMathhman <minecraftmathhman@gmail.com>
2019-02-28 19:45:26 -0500

    Add Streams

--- src/main/java/com/meti/lib/net/Client.java
+++ src/main/java/com/meti/lib/net/Client.java
@@ -1,5 +1,8 @@
 package com.meti.lib.net;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
@@ -9,8 +12,16 @@ import java.net.Socket;
  */
 public class Client {
     private final Socket socket;
+    private final ObjectInputStream inputStream;
+    private final ObjectOutputStream outputStream;
 
-    public Client(Socket socket) {
+    public Client(Socket socket) throws IOException {
         this.socket = socket;
+
+        //these two items are reversed b/c the ObjectInputStream must flush the header
+        this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+        this.inputStream = new ObjectInputStream(socket.getInputStream());
     }
+
+
 }
