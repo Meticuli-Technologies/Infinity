c2b262979903f8a9f3857acf016083602b9ec433
SirMathhman <minecraftmathhman@gmail.com>
2019-03-02 14:35:06 -0500

    Add skeleton to ServerMenu

--- src/main/java/com/meti/app/ServerMenu.java
+++ src/main/java/com/meti/app/ServerMenu.java
@@ -1,9 +1,24 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 2/24/2019
  */
 public class ServerMenu {
+    @FXML
+    private TextField portField;
+
+    @FXML
+    public void back(){
+
+    }
+
+    @FXML
+    public void next(){
+
+    }
 }
