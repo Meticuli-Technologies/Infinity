98b3f9e0dfc87c60b84e2bb7da3762b1f8a2ae36
SirMathhman <minecraftmathhman@gmail.com>
2019-03-02 14:39:09 -0500

    Add skeleton to ServerDisplay

--- src/main/java/com/meti/app/ServerDisplay.java
+++ src/main/java/com/meti/app/ServerDisplay.java
@@ -1,9 +1,14 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/2/2019
  */
 public class ServerDisplay {
+    @FXML
+    private ListView<String> clientListView;
 }
