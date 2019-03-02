60b7852d4be98c5778f32ac52ff17e846301b250
SirMathhman <minecraftmathhman@gmail.com>
2019-03-02 14:37:08 -0500

    Partially implement ServerMenu.next

--- src/main/java/com/meti/app/ServerMenu.java
+++ src/main/java/com/meti/app/ServerMenu.java
@@ -5,6 +5,10 @@ import com.meti.lib.fx.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
@@ -29,6 +33,12 @@ public class ServerMenu extends Controller {
 
     @FXML
     public void next(){
-
+   /*     try {
+            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(portField.getText()));
+            ServerDisplay menu = ontoURL(getClass().getResource("/com/meti/app/ServerDisplay.fxml"));
+            menu.load(serverSocket);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
 }
