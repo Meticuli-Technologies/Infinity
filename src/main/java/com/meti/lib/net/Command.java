ca0f42dbd4152790f60bcc5aaadaacb7d0d2b079
SirMathhman <minecraftmathhman@gmail.com>
2019-03-02 15:14:21 -0500

    Make Command implement Serializable

--- src/main/java/com/meti/lib/net/Command.java
+++ src/main/java/com/meti/lib/net/Command.java
@@ -1,11 +1,13 @@
 package com.meti.lib.net;
 
+import java.io.Serializable;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 2/28/2019
  */
-public class Command {
+public class Command implements Serializable {
     private final String[] args;
 
     public Command(String... args) {
