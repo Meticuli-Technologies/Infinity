b9cf2d069851d375f7d8783439059dd66f15d57b
SirMathhman <sirmathhman@gmail.com>
2019-03-04 14:05:34 -0500

    Change args from String[] to List<String>

--- src/main/java/com/meti/lib/net/Command.java
+++ src/main/java/com/meti/lib/net/Command.java
@@ -1,6 +1,10 @@
 package com.meti.lib.net;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author SirMathhman
@@ -8,14 +12,18 @@ import java.io.Serializable;
  * @since 2/28/2019
  */
 public class Command implements Serializable {
-    public final String[] args;
+    public final List<String> args = new ArrayList<>();
 
     public Command(String... args) {
-        this.args = args;
+        this.args.addAll(Arrays.asList(args));
+    }
+
+    public Command(Collection<String> args) {
+        this.args.addAll(args);
     }
 
     @Override
     public String toString() {
-        return String.join(" ", args);
+        return String.join(" ", args.toArray(new String[0]));
     }
 }
