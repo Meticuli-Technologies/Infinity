683adfcd95719f620db347047eb67821e87f95c1
SirMathhman <sirmathhman@gmail.com>
2019-03-04 14:03:58 -0500

    Add state to InfinityServer

--- src/main/java/com/meti/app/InfinityServer.java
+++ src/main/java/com/meti/app/InfinityServer.java
@@ -1,11 +1,17 @@
 package com.meti.app;
 
+import com.meti.lib.State;
 import com.meti.lib.net.Client;
 import com.meti.lib.net.ClientHandler;
 import com.meti.lib.net.Command;
 import com.meti.lib.net.Server;
+import com.meti.lib.net.token.TokenHandler;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.ServerSocket;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -14,6 +20,8 @@ import java.util.function.Consumer;
  * @since 3/3/2019
  */
 class InfinityServer extends Server {
+    private final State state = new State();
+
     public InfinityServer(ServerSocket serverSocket) {
         super(serverSocket);
     }
@@ -25,14 +33,13 @@ class InfinityServer extends Server {
         return handler;
     }
 
-    private class CommandHandler extends com.meti.lib.net.token.TokenHandler<Command> {
+    private class CommandHandler extends TokenHandler<Command> {
         public CommandHandler() {
             super(Command.class);
         }
 
         @Override
         public void accept(Command command) {
-            System.out.println(command);
         }
     }
 }
--- src/main/java/com/meti/lib/net/Command.java
+++ src/main/java/com/meti/lib/net/Command.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
  * @since 2/28/2019
  */
 public class Command implements Serializable {
-    private final String[] args;
+    public final String[] args;
 
     public Command(String... args) {
         this.args = args;
