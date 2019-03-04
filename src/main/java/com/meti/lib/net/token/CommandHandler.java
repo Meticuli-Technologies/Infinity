8c33e45d9b3cad83e46e25e0214f0e50893d6ebe
SirMathhman <sirmathhman@gmail.com>
2019-03-04 14:17:43 -0500

    Add map to CommandHandler

--- src/main/java/com/meti/lib/net/token/CommandHandler.java
+++ src/main/java/com/meti/lib/net/token/CommandHandler.java
@@ -2,13 +2,31 @@ package com.meti.lib.net.token;
 
 import com.meti.lib.net.Command;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class CommandHandler extends TypeHandler<Command> {
+    private final Map<Predicate<Command>, Consumer<Command>> map = new HashMap<>();
+
     public CommandHandler() {
         super(Command.class);
     }
 
     @Override
     public void acceptCast(Command obj) {
+        List<Consumer<Command>> list = map.keySet()
+                .stream()
+                .filter(commandPredicate -> commandPredicate.test(obj))
+                .map(map::get)
+                .peek(commandConsumer -> commandConsumer.accept(obj))
+                .collect(Collectors.toList());
 
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException(obj + " is an unprocessable command");
+        }
     }
 }
