6572bc4034fa0f5c015260946eb9f816e18b5356
SirMathhman <sirmathhman@gmail.com>
2019-03-08 14:33:18 -0500

    Unextend State of ArrayList<String> and transform into field

--- src/main/java/com/meti/lib/State.java
+++ src/main/java/com/meti/lib/State.java
@@ -6,13 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 2/24/2019
  */
-public class State extends ArrayList<Object> {
+public class State {
+    private final List<Object> list = new ArrayList<>();
+
+    public void add(Object obj) {
+    }
+
     public <T> Optional<T> byClassToSingle(Class<T> tClass) throws Exception {
         List<T> list = byClass(tClass);
         return CollectionUtil.toSingle(list);
@@ -20,7 +26,7 @@ public class State extends ArrayList<Object> {
 
     public <T> List<T> byClass(Class<T> tClass) throws Exception {
         TryableFactory factory = new TryableFactory();
-        return factory.checkAll(stream()
+        return factory.checkAll(list.stream()
                 .filter(o -> tClass.isAssignableFrom(o.getClass()))
                 .map(factory.apply(tClass::cast))
                 .flatMap(OptionalUtil::stream)
@@ -29,7 +35,11 @@ public class State extends ArrayList<Object> {
 
     public State createSubState() {
         State state = new State();
-        add(state);
+        list.add(state);
         return state;
+    }
+
+    public Stream<Object> stream() {
+        return Stream.empty();
     }
 }
