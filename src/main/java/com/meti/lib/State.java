79b6ea5114ca9431cd71ee93b014022733d7ac62
SirMathhman <sirmathhman@gmail.com>
2019-03-08 14:35:51 -0500

    Change List<Object> to HandlerMap<Object>

--- src/main/java/com/meti/lib/State.java
+++ src/main/java/com/meti/lib/State.java
@@ -1,11 +1,11 @@
 package com.meti.lib;
 
+import com.meti.lib.handle.HandlerMap;
 import com.meti.lib.util.CollectionUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
  * @since 2/24/2019
  */
 public class State {
-    private final List<Object> list = new ArrayList<>();
+    private final HandlerMap<Object> map = new HandlerMap<>();
 
     public void add(Object obj) {
     }
@@ -26,16 +26,17 @@ public class State {
 
     public <T> List<T> byClass(Class<T> tClass) throws Exception {
         TryableFactory factory = new TryableFactory();
-        return factory.checkAll(list.stream()
+/*        return factory.checkAll(list.stream()
                 .filter(o -> tClass.isAssignableFrom(o.getClass()))
                 .map(factory.apply(tClass::cast))
                 .flatMap(OptionalUtil::stream)
-                .collect(Collectors.toList()));
+                .collect(Collectors.toList()));*/
+        return new ArrayList<>();
     }
 
     public State createSubState() {
         State state = new State();
-        list.add(state);
+        /*        map.add(state);*/
         return state;
     }
 
