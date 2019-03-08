ac02944921227dcc57bc51e6fc6fde3d2e4c54fa
SirMathhman <sirmathhman@gmail.com>
2019-03-08 14:16:32 -0500

    Change to ObservableList

--- src/main/java/com/meti/app/Chat.java
+++ src/main/java/com/meti/app/Chat.java
@@ -2,18 +2,22 @@ package com.meti.app;
 
 import com.meti.lib.net.Command;
 import com.meti.lib.net.token.CommandHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
-public class Chat extends ArrayList<String> {
+public class Chat {
+    private final ObservableList<String> messages = FXCollections.observableArrayList();
+
     public static class Add extends CommandHandler.CommandHandlerImpl {
         @Override
         public void accept(Command command) {
             try {
                 state.byClassToSingle(Chat.class)
                         .orElseThrow(() -> new NoSuchElementException("Chat not found"))
-                        .add(command.args.get(2));
+                        .messages.add(command.args.get(2));
             } catch (Exception e) {
                 e.printStackTrace();
             }
