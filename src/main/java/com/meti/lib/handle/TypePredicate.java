dfc45f2e6418686a035453053f924487d7cecda9
SirMathhman <sirmathhman@gmail.com>
2019-03-08 14:42:20 -0500

    Add TypePredicate

---
+++ src/main/java/com/meti/lib/handle/TypePredicate.java
@@ -0,0 +1,29 @@
+package com.meti.lib.handle;
+
+import java.util.function.Predicate;
+
+public class TypePredicate<T> implements Predicate<Object> {
+    private final Class<T> tClass;
+    private boolean equal = false;
+
+    public TypePredicate(Class<T> tClass) {
+        this.tClass = tClass;
+    }
+
+    public void setEqual(boolean equal) {
+        this.equal = equal;
+    }
+
+    @Override
+    public boolean test(Object o) {
+        if (isEqual()) {
+            return tClass.equals(o.getClass());
+        } else {
+            return tClass.isAssignableFrom(o.getClass());
+        }
+    }
+
+    public boolean isEqual() {
+        return equal;
+    }
+}
