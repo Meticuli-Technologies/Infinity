e48a73e8adcf47364ca82a876ab43557572c2237
SirMathhman <sirmathhman@gmail.com>
2019-03-08 14:44:03 -0500

    Add CollectionConsumer

---
+++ src/main/java/com/meti/lib/handle/CollectionConsumer.java
@@ -0,0 +1,23 @@
+package com.meti.lib.handle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class CollectionConsumer<T, C extends Collection<T>> implements Consumer<T> {
+    private final C collection;
+
+    public CollectionConsumer(C collection) {
+        this.collection = collection;
+    }
+
+    public static <T> CollectionConsumer<T, List<T>> asList() {
+        return new CollectionConsumer<>(new ArrayList<>());
+    }
+
+    @Override
+    public void accept(T t) {
+        collection.add(t);
+    }
+}
