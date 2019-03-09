86788a0dfdbc649e754ebc6dcce425c8dc9831e1
SirMathhman <minecraftmathhman@gmail.com>
2019-03-09 13:49:03 -0500

    Realize that a CompoundHandler is a wrapper class for HandlerMap

--- src/main/java/com/meti/lib/handle/HandlerMap.java
+++ src/main/java/com/meti/lib/handle/HandlerMap.java
@@ -2,5 +2,15 @@ package com.meti.lib.handle;
 
 import java.util.HashSet;
 
-public class HandlerMap<T, H extends Handler<T>> extends HashSet<H> {
+public class HandlerMap<T, H extends Handler<T>> extends HashSet<H> implements Handler<T> {
+    @Override
+    public void accept(T t) {
+        stream().filter(h -> h.test(t))
+                .forEach(h -> h.accept(t));
+    }
+
+    @Override
+    public boolean test(T t) {
+        return stream().anyMatch(h -> h.test(t));
+    }
 }
