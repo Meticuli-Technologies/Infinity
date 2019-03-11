56708b443078ba77bac706286eb59d044732bc79
SirMathhman <minecraftmathhman@gmail.com>
2019-03-11 17:30:01 -0400

    Skeleton ServerDisplay

--- src/main/java/com/meti/ServerDisplay.java
+++ src/main/java/com/meti/ServerDisplay.java
@@ -1,9 +1,20 @@
 package com.meti;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/11/2019
  */
 public class ServerDisplay {
+    @FXML
+    private ListView<String> clientListView;
+
+    @FXML
+    private TextArea chatArea;
+
+
 }
