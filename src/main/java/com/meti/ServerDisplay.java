5c819ba271da2ae6bd117ffe92f07c5b81184d16
SirMathhman <minecraftmathhman@gmail.com>
2019-03-11 17:37:37 -0400

    Add ServerSocket

--- src/main/java/com/meti/ServerDisplay.java
+++ src/main/java/com/meti/ServerDisplay.java
@@ -5,6 +5,11 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.ServerSocket;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
@@ -24,8 +29,36 @@ public class ServerDisplay {
     public void handleInput() {
         String command = input.getText();
         if (!command.startsWith("/")) {
-            logMessage("server", command);
+            logMessage(command);
         }
+
+        String withoutSlash = command.replace('/', ' ');
+        String[] arguments = withoutSlash.split(" ");
+        if (arguments[0].equals("start")) {
+            int port = Integer.parseInt(arguments[1]);
+            logMessage("Starting server on " + port);
+
+            try {
+                ServerSocket serverSocket = new ServerSocket(port);
+            } catch (IOException e) {
+                logMessage("Failed to start server on " + port, e);
+            }
+        }
+    }
+
+    public void logMessage(String name) {
+        logMessage("server", name);
+    }
+
+    public void logMessage(String message, Exception exception) {
+        logMessage(message);
+        logMessage(exception);
+    }
+
+    public void logMessage(Exception exception) {
+        StringWriter writer = new StringWriter();
+        exception.printStackTrace(new PrintWriter(writer));
+        logMessage(writer.toString());
     }
 
     public void logMessage(String user, String name){
