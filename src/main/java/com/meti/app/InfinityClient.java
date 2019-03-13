62a7b3fd8c0bfb01a7b0f3f8222812bcf515243d
SirMathhman <minecraftmathhman@gmail.com>
2019-03-12 20:29:15 -0400

    Implement InfinityClient

--- src/main/java/com/meti/app/InfinityClient.java
+++ src/main/java/com/meti/app/InfinityClient.java
@@ -1,9 +1,22 @@
 package com.meti.app;
 
+import com.meti.lib.Client;
+
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/12/2019
  */
-public class InfinityClient {
+public class InfinityClient extends Client  {
+    public InfinityClient(Socket socket) throws IOException {
+        super(socket);
+    }
+
+    @Override
+    protected void handleObject(Object token) {
+
+    }
 }
