b73805e6222c814010daa15219f6b985e8b37877
SirMathhman <sirmathhman@gmail.com>
2019-03-13 16:19:20 -0400

    Add convenience methods

--- src/main/java/com/meti/lib/Console.java
+++ src/main/java/com/meti/lib/Console.java
@@ -4,18 +4,22 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public abstract class Console {
-    public void log(User user, Level level, String message, Exception exception){
+    public void log(User user, Level level, Exception exception) {
+        log(user, level, null, exception);
+    }
+
+    public void log(User user, Level level, String message, Exception exception) {
         StringBuilder builder = new StringBuilder();
         builder.append("[" + user.name + "," + level.name() + "]: ");
-        if(message != null){
+        if (message != null) {
             builder.append(message);
         }
 
-        if(message != null && exception != null){
+        if (message != null && exception != null) {
             builder.append("\n");
         }
 
-        if(exception != null){
+        if (exception != null) {
             StringWriter writer = new StringWriter();
             exception.printStackTrace(new PrintWriter(writer));
             builder.append(writer.toString());
@@ -25,4 +29,8 @@ public abstract class Console {
     }
 
     public abstract void handle(String message);
+
+    public void log(User user, Level level, String message) {
+        log(user, level, message, null);
+    }
 }
