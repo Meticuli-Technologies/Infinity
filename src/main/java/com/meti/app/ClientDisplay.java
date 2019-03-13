c1086687a8f74fc674127e0594a9643616e43875
SirMathhman <sirmathhman@gmail.com>
2019-03-13 15:34:20 -0400

    Add skeleton to clientDisplay

--- src/main/java/com/meti/app/ClientDisplay.java
+++ src/main/java/com/meti/app/ClientDisplay.java
@@ -1,9 +1,23 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/11/2019
  */
 public class ClientDisplay {
+    @FXML
+    private TextField input;
+
+    @FXML
+    private TextArea output;
+
+    @FXML
+    public void handleInput(){
+
+    }
 }
