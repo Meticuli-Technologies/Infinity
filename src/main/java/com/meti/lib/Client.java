9529f60d35d6c0e3d8e1779fcf8c68f292c67a70
SirMathhman <minecraftmathhman@gmail.com>
2019-03-12 20:05:33 -0400

    Implement Client

--- src/main/java/com/meti/lib/Client.java
+++ src/main/java/com/meti/lib/Client.java
@@ -1,9 +1,48 @@
 package com.meti.lib;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/12/2019
  */
-public class Client {
+public class Client implements Closeable {
+    private final Socket socket;
+    private final ObjectInputStream inputStream;
+    private final ObjectOutputStream outputStream;
+
+    public Client(Socket socket) throws IOException {
+        this.socket = socket;
+
+        this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+        this.inputStream = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void close() throws IOException {
+        socket.close();
+
+        inputStream.close();
+        outputStream.close();
+    }
+
+    public void flush() throws IOException {
+        outputStream.flush();
+    }
+
+    public Object readObject() throws IOException, ClassNotFoundException {
+        return inputStream.readObject();
+    }
+
+    public void writeObject(Object obj) throws IOException {
+        outputStream.writeObject(obj);
+    }
+
+    public void writeUnshared(Object obj) throws IOException {
+        outputStream.writeUnshared(obj);
+    }
 }
