dce1e33902b07ff315a4761d746c96ba640559c8
SirMathhman <sirmathhman@gmail.com>
2019-03-18 14:29:26 -0400

    Implement init

--- src/main/java/com/meti/app/ServerMain.java
+++ src/main/java/com/meti/app/ServerMain.java
@@ -1,8 +1,15 @@
 package com.meti.app;
 
+import com.meti.lib.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class ServerMain {
+    private Server server;
+
     public static void main(String[] args) {
         ServerMain main = new ServerMain();
         Scanner scanner = new Scanner(System.in);
@@ -15,6 +22,28 @@ public class ServerMain {
     }
 
     private void init(Scanner scanner) {
+        try {
+            int port = getPort(scanner);
+            this.server = new Server(new ServerSocket(port)) {
+                @Override
+                public void handleAccept(Socket accept) throws Exception {
 
+                }
+            };
+        } catch (IOException e) {
+            System.out.println("Failed to initialize. " + e.getMessage());
+        }
+    }
+
+    private int getPort(Scanner scanner) {
+        while (true) {
+            System.out.print("Enter in the port: ");
+            String portToken = scanner.next();
+            try {
+                return Integer.parseInt(portToken);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port: " + portToken);
+            }
+        }
     }
 }
