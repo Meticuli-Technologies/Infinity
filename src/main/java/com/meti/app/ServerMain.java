8232ba52b3671346b13dbadb1f5b447c4243c955
SirMathhman <sirmathhman@gmail.com>
2019-03-18 14:23:43 -0400

    Add init

--- src/main/java/com/meti/app/ServerMain.java
+++ src/main/java/com/meti/app/ServerMain.java
@@ -1,4 +1,15 @@
 package com.meti.app;
 
+import java.util.Scanner;
+
 public class ServerMain {
+    public static void main(String[] args) {
+        ServerMain main = new ServerMain();
+        Scanner scanner = new Scanner(System.in);
+        main.init(scanner);
+    }
+
+    public void init(Scanner scanner){
+
+    }
 }
