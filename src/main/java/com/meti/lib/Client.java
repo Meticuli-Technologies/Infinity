9c02e2205a86a0de2ad081c215cad081842a25f6
SirMathhman <minecraftmathhman@gmail.com>
2019-03-19 15:53:37 -0400

    Remove bad methods

--- src/main/java/com/meti/lib/Client.java
+++ src/main/java/com/meti/lib/Client.java
@@ -2,16 +2,12 @@ package com.meti.lib;
 
 import java.io.*;
 import java.net.Socket;
-import java.time.Duration;
 import java.util.Collection;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
 
 public class Client implements Closeable {
     private final Socket socket;
     private final ObjectInputStream inputStream;
     private final ObjectOutputStream outputStream;
-    public static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(10);
 
     public Client(Socket socket) throws IOException {
         this.socket = socket;
@@ -34,21 +30,8 @@ public class Client implements Closeable {
         return socket;
     }
 
-    public <R extends Response> R query(Respondable<R> respondable) throws Exception {
-        return query(respondable, DEFAULT_TIMEOUT);
-    }
-
-    private <R extends Response> R query(Respondable<R> respondable, Duration timeout) throws Exception {
-        write(respondable);
-
-        Class<? extends R> responseClass = respondable.getResponseClass();
-
-        Object token = read(timeout);
-        if (responseClass.isAssignableFrom(token.getClass())) {
-            return responseClass.cast(token);
-        } else {
-            throw new IllegalStateException("Invalid token: " + token);
-        }
+    public Object read() throws IOException, ClassNotFoundException {
+        return inputStream.readObject();
     }
 
     public void write(Serializable serializable) throws IOException {
@@ -57,18 +40,10 @@ public class Client implements Closeable {
     }
 
     public void write(Collection<? extends Serializable> serializables) throws IOException {
-        for(Serializable serializable : serializables){
+        for (Serializable serializable : serializables) {
             outputStream.writeObject(serializable);
         }
 
         outputStream.flush();
-    }
-
-    public Object read() throws Exception {
-        return read(DEFAULT_TIMEOUT);
-    }
-
-    private Object read(Duration timeout) throws Exception {
-        return new FutureTask<>(inputStream::readObject).get(timeout.toMillis(), TimeUnit.MILLISECONDS);
     }
 }
