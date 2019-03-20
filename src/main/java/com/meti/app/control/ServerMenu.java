591e56c3fbb124e73bce2c9f7411e6d6ad6c504c
SirMathhman <sirmathhman@gmail.com>
2019-03-20 16:05:16 -0400

    Add Controller to ServerMenu

--- src/main/java/com/meti/app/control/ServerMenu.java
+++ src/main/java/com/meti/app/control/ServerMenu.java
@@ -1,11 +1,17 @@
 package com.meti.app.control;
 
+import com.meti.lib.State;
+import com.meti.lib.fx.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
-public class ServerMenu {
+public class ServerMenu extends Controller {
     @FXML
     private TextField portField;
+
+    public ServerMenu(State state) {
+        super(state);
+    }
 
     @FXML
     public void exit(){
