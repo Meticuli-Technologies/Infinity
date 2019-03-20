8508dac30e0deb9cc22445ff1b793af89c6b8aa6
SirMathhman <sirmathhman@gmail.com>
2019-03-20 15:58:25 -0400

    Add skeleton

--- src/main/java/com/meti/app/control/ServerMenu.java
+++ src/main/java/com/meti/app/control/ServerMenu.java
@@ -1,4 +1,19 @@
 package com.meti.app.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class ServerMenu {
+    @FXML
+    private TextField portField;
+
+    @FXML
+    public void exit(){
+
+    }
+
+    @FXML
+    public void next(){
+
+    }
 }
