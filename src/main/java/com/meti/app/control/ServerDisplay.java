9b5bdf1211e9f315adf6c113c38412162046270c
SirMathhman <sirmathhman@gmail.com>
2019-03-20 16:23:51 -0400

    Skeleton

--- src/main/java/com/meti/app/control/ServerDisplay.java
+++ src/main/java/com/meti/app/control/ServerDisplay.java
@@ -1,4 +1,23 @@
 package com.meti.app.control;
 
-public class ServerDisplay {
+import com.meti.lib.State;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServerDisplay extends InfinityController implements Initializable {
+    @FXML
+    private ListView<String> clientsView;
+
+    public ServerDisplay(State state) {
+        super(state);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
