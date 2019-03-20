47a3f70af3841bb4bdc93231bbd964e97cc60f84
SirMathhman <sirmathhman@gmail.com>
2019-03-20 16:02:05 -0400

    Add Controller

---
+++ src/main/java/com/meti/lib/fx/Controller.java
@@ -0,0 +1,11 @@
+package com.meti.lib.fx;
+
+import com.meti.lib.State;
+
+public class Controller {
+    protected final State state;
+
+    public Controller(State state) {
+        this.state = state;
+    }
+}
