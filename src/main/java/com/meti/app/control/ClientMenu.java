f0cd5b389433b14ccbf60b079e79f0a68995b8fa
SirMathhman <sirmathhman@gmail.com>
2019-03-21 10:45:05 -0400

    Skeleton

--- src/main/java/com/meti/app/control/ClientMenu.java
+++ src/main/java/com/meti/app/control/ClientMenu.java
@@ -1,4 +1,23 @@
 package com.meti.app.control;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class ClientMenu {
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField portField;
+
+    @FXML
+    public void exit(){
+        Platform.exit();
+    }
+
+    @FXML
+    public void next(){
+        //TODO: implement next
+    }
 }
