954f7bc13ffa48830745fb6d7b423bfd751d6f58
SirMathhman <sirmathhman@gmail.com>
2019-03-21 11:09:07 -0400

    Add getclient

--- src/main/java/com/meti/app/control/InfinityController.java
+++ src/main/java/com/meti/app/control/InfinityController.java
@@ -2,9 +2,8 @@ package com.meti.app.control;
 
 import com.meti.lib.State;
 import com.meti.lib.fx.Controller;
+import com.meti.lib.net.Client;
 import com.meti.lib.net.Server;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
@@ -16,6 +15,10 @@ public class InfinityController extends Controller {
         super(state);
 
         this.service = state.byClassToSingle(ExecutorService.class).orElseThrow();
+    }
+
+    public Optional<Client> getClient() {
+        return state.byClassToSingle(Client.class);
     }
 
     public Optional<Server> getServer() {
