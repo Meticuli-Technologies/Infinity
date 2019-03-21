b788973082e56e6f2ccc358c10b01732cc8af8f9
SirMathhman <sirmathhman@gmail.com>
2019-03-21 11:09:51 -0400

    Add initialization

--- src/main/java/com/meti/app/control/ClientDisplay.java
+++ src/main/java/com/meti/app/control/ClientDisplay.java
@@ -1,18 +1,33 @@
 package com.meti.app.control;
 
+import com.meti.lib.State;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ClientDisplay {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientDisplay extends InfinityController implements Initializable {
     @FXML
     private TextArea output;
 
     @FXML
     private TextField input;
 
-    @FXML
-    public void handle(){
+    public ClientDisplay(State state) {
+        super(state);
+    }
 
+
+    @FXML
+    public void handle() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        output.appendText("Successfully logged on.");
     }
 }
