30d4a6e6242b998c80b7891ffd11c555628fbadc
SirMathhman <sirmathhman@gmail.com>
2019-03-21 11:08:27 -0400

    Implement next

--- src/main/java/com/meti/app/control/ClientMenu.java
+++ src/main/java/com/meti/app/control/ClientMenu.java
@@ -1,23 +1,41 @@
 package com.meti.app.control;
 
+import com.meti.lib.State;
+import com.meti.lib.net.Client;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
-public class ClientMenu {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClientMenu extends InfinityController {
     @FXML
     private TextField addressField;
 
     @FXML
     private TextField portField;
 
+    public ClientMenu(State state) {
+        super(state);
+    }
+
     @FXML
-    public void exit(){
+    public void exit() {
         Platform.exit();
     }
 
     @FXML
-    public void next(){
+    public void next() {
         //TODO: implement next
+        try {
+            Socket socket = new Socket(InetAddress.getByName(addressField.getText()), Integer.parseInt(portField.getText()));
+            Client client = new Client(socket);
+
+            state.add(client);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
