72698cd97f30990d295dd436147a086cb4725de5
SirMathhman <sirmathhman@gmail.com>
2019-03-21 11:00:06 -0400

    Hook up Menu to other Menus

--- src/main/java/com/meti/app/control/Menu.java
+++ src/main/java/com/meti/app/control/Menu.java
@@ -3,6 +3,8 @@ package com.meti.app.control;
 import com.meti.lib.State;
 import javafx.fxml.FXML;
 
+import java.io.IOException;
+
 public class Menu extends InfinityController {
     public Menu(State state) {
         super(state);
@@ -10,11 +12,19 @@ public class Menu extends InfinityController {
 
     @FXML
     public void connect() {
-
+        try {
+            onto(getClass().getResource("/com/meti/app/control/ClientMenu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     public void host() {
-
+        try {
+            onto(getClass().getResource("/com/meti/app/control/ServerMenu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
