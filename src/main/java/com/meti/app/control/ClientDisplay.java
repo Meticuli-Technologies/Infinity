55b07e063bb1d0b8c76c98b622afe08aca44cbe5
SirMathhman <sirmathhman@gmail.com>
2019-03-21 10:47:15 -0400

    Skeleton

--- src/main/java/com/meti/app/control/ClientDisplay.java
+++ src/main/java/com/meti/app/control/ClientDisplay.java
@@ -1,4 +1,18 @@
 package com.meti.app.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ClientDisplay {
+    @FXML
+    private TextArea output;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    public void handle(){
+
+    }
 }
