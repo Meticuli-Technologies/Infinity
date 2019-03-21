60eb5cff024774fa56d2f6bf0d6db29461c114ee
SirMathhman <minecraftmathhman@gmail.com>
2019-03-21 16:59:23 -0400

    Skeleton Login

--- src/main/java/com/meti/app/control/Login.java
+++ src/main/java/com/meti/app/control/Login.java
@@ -1,9 +1,24 @@
 package com.meti.app.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/21/2019
  */
 public class Login {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    public void back(){
+
+    }
+
+    @FXML
+    public void next(){
+
+    }
 }
