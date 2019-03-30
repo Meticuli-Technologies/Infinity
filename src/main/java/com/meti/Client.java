5ac18fb9911cd6e661d0341ce8aa54a72ef4f51b
SirMathhman <minecraftmathhman@gmail.com>
2019-03-30 19:35:10 -0400

    Add ObjectClient

--- src/main/java/com/meti/Client.java
+++ src/main/java/com/meti/Client.java
@@ -2,23 +2,17 @@ package com.meti;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/30/2019
  */
-public class Client<I extends InputStream, O extends OutputStream> implements Closeable {
-    private final Source<I, O> source;
-    private final I inputStream;
-    private final O outputStream;
+public class Client<S extends Source<?, ?>> implements Closeable {
+    private final S source;
 
-    public Client(Source<I, O> source) throws IOException {
+    public Client(S source) {
         this.source = source;
-        this.inputStream = source.getInputStream();
-        this.outputStream = source.getOutputStream();
     }
 
     @Override
---
+++ src/main/java/com/meti/ObjectClient.java
@@ -0,0 +1,12 @@
+package com.meti;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 3/30/2019
+ */
+public class ObjectClient extends Client<ObjectSource> {
+    public ObjectClient(ObjectSource source) {
+        super(source);
+    }
+}
