6a920682f5e7295a9af18158ea342959c764e891
SirMathhman <minecraftmathhman@gmail.com>
2019-03-30 19:07:09 -0400

    Add ServerSocket

--- src/main/java/com/meti/ServerMain.java
+++ src/main/java/com/meti/ServerMain.java
@@ -1,5 +1,9 @@
 package com.meti;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Scanner;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
@@ -7,6 +11,12 @@ package com.meti;
  */
 public class ServerMain {
     public static void main(String[] args) {
-
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int port = Integer.parseInt(scanner.nextLine());
+            ServerSocket serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
