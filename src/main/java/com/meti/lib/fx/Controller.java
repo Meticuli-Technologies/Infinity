95ed8706a40d37d755f81157e84277e5b6ef5a6d
SirMathhman <minecraftmathhman@gmail.com>
2019-03-31 15:26:15 -0400

    Add convenience methods

--- src/main/java/com/meti/lib/fx/Controller.java
+++ src/main/java/com/meti/lib/fx/Controller.java
@@ -1,6 +1,13 @@
 package com.meti.lib.fx;
 
 import com.meti.lib.collection.State;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
 
 /**
  * @author SirMathhman
@@ -12,5 +19,25 @@ public class Controller {
 
     public Controller(State state) {
         this.state = state;
+    }
+
+    public void onto(URL url) throws IOException {
+        onto((Parent) ControllerLoader.load(url, state));
+    }
+
+    public void onto(Parent parent) {
+        Optional<Stage> any = state.byClass(Stage.class).findAny();
+        Stage toSet;
+        if (any.isPresent()) {
+            toSet = any.get();
+        } else {
+            toSet = new Stage();
+            state.add(toSet);
+        }
+
+        toSet.setScene(new Scene(parent));
+        if (!toSet.isShowing()) {
+            toSet.show();
+        }
     }
 }
