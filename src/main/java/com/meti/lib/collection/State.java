c896562d7975fc6613e3a0b48db024871e2296a6
SirMathhman <minecraftmathhman@gmail.com>
2019-03-31 13:39:35 -0400

    Add State

---
+++ src/main/java/com/meti/lib/collection/State.java
@@ -0,0 +1,21 @@
+package com.meti.lib.collection;
+
+import java.util.HashSet;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 3/31/2019
+ */
+public class State extends HashSet<Object> {
+    public <T> Stream<T> search(Class<T> tClass) {
+        return search(new TypePredicate<>(tClass))
+                .map(new TypeFunction<>(tClass));
+    }
+
+    public Stream<Object> search(Predicate<Object> predicate) {
+        return stream().filter(predicate);
+    }
+}
