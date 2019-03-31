4a502faac1da4547ae43ab4f3bc615a5365fa0ba
SirMathhman <minecraftmathhman@gmail.com>
2019-03-31 13:08:20 -0400

    Add Menu.fxml

---
+++ src/main/java/com/meti/app/control/Menu.java
@@ -0,0 +1,34 @@
+package com.meti.app.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 3/31/2019
+ */
+public class Menu {
+    @FXML
+    private TextField versionText;
+
+    @FXML
+    public void connect(){
+
+    }
+
+    @FXML
+    public void host(){
+
+    }
+
+    @FXML
+    public void local(){
+
+    }
+
+    @FXML
+    public void settings(){
+
+    }
+}
