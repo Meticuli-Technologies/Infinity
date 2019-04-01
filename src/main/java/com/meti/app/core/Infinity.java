2e123e22ed38f052c62ee0b2e9da5a3429ce4881
SirMathhman <sirmathhman@gmail.com>
2019-04-01 14:11:31 -0400

    Add service

--- src/main/java/com/meti/app/core/Infinity.java
+++ src/main/java/com/meti/app/core/Infinity.java
@@ -7,7 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
 
 import static com.meti.lib.fx.ControllerLoader.load;
 import static com.meti.lib.trys.TryableFactory.DEFAULT_FACTORY;
@@ -18,12 +22,14 @@ import static com.meti.lib.trys.TryableFactory.DEFAULT_FACTORY;
  * @since 3/31/2019
  */
 public class Infinity {
+    private final ExecutorService service = Executors.newCachedThreadPool();
     private final Console console = new Console();
     private final State state = new State();
 
     public void start(Stage primaryStage){
         console.log(Level.INFO, "Setting up state.");
         state.add(primaryStage);
+        state.add(service);
         state.add(console);
 
         try {
@@ -41,6 +47,22 @@ public class Infinity {
             state.byClass(Closeable.class)
                     .forEach(DEFAULT_FACTORY.newConsumer(Closeable::close));
             DEFAULT_FACTORY.catcher.throwAll();
+
+            service.shutdown();
+            if(!service.isTerminated()){
+                List<Runnable> runnables = service.shutdownNow();
+                String runnableList = runnables
+                        .stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining("\n\t"));
+
+                if(runnables.isEmpty()){
+                    console.log(Level.INFO, "Shutdown server successfully.");
+                }
+                else{
+                    console.log(Level.SEVERE, runnables.size() + " runnables were still running:\n\t" + runnableList);
+                }
+            }
         } catch (Exception e) {
             console.log(Level.SEVERE, "Failed to stop Infinity.", e);
         }
