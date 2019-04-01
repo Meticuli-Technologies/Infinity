eb3f57ff4428a7c026c8824fe8167ae43acd7aa8
SirMathhman <sirmathhman@gmail.com>
2019-04-01 14:16:07 -0400

    Extend InfinityController

--- src/main/java/com/meti/app/control/ClientDisplay.java
+++ src/main/java/com/meti/app/control/ClientDisplay.java
@@ -1,13 +1,20 @@
 package com.meti.app.control;
 
+import com.meti.app.core.InfinityController;
+import com.meti.lib.collection.State;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class ClientDisplay {
+public class ClientDisplay extends InfinityController {
+
     @FXML
     private TextField input;
 
     @FXML
     private ListView<String> output;
+
+    public ClientDisplay(State state) {
+        super(state);
+    }
 }
--- src/main/java/com/meti/app/control/ServerDisplay.java
+++ src/main/java/com/meti/app/control/ServerDisplay.java
@@ -1,12 +1,19 @@
 package com.meti.app.control;
 
+import com.meti.app.core.InfinityController;
+import com.meti.lib.collection.State;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
-public class ServerDisplay {
+public class ServerDisplay extends InfinityController {
+
     @FXML
     private ListView<String> clientList;
 
     @FXML
     private ListView<String> chatList;
+
+    public ServerDisplay(State state) {
+        super(state);
+    }
 }
