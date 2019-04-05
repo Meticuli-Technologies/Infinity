f9d0d8fb0c96361771cec38550ef6cbe7ba9b62c
SirMathhman <minecraftmathhman@gmail.com>
2019-04-05 14:46:55 -0400

    Refactor to correct implementation

--- src/main/java/com/meti/app/Infinity.java
+++ src/main/java/com/meti/app/Infinity.java
@@ -8,11 +8,23 @@ import javafx.stage.Stage;
  * @since 4/5/2019
  */
 class Infinity implements InfinityImpl {
+    private boolean running;
+
     @Override
     public void start(Stage primaryStage) {
+        if(running){
+            throw new IllegalStateException("Infinity is already running!");
+        }
+
+        this.running = true;
     }
 
     @Override
     public void stop() {
+        if(!running){
+            throw new IllegalStateException("Infinity is not running yet!");
+        }
+
+        this.running = false;
     }
 }
