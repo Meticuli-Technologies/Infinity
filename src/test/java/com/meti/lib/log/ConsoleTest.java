3991d909490a2f040f6ac9a8d89ca8674211dc38
SirMathhman <minecraftmathhman@gmail.com>
2019-04-05 15:42:40 -0400

    Comment out code for future testing

--- src/test/java/com/meti/lib/log/ConsoleTest.java
+++ src/test/java/com/meti/lib/log/ConsoleTest.java
@@ -2,6 +2,10 @@ package com.meti.lib.log;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.BiConsumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
@@ -10,11 +14,31 @@ import org.junit.jupiter.api.Test;
 public class ConsoleTest {
     @Test
     void log() {
-        Console console = new Console.LoggerConsole();
+      /* Console.LoggerConsole console = new Console.LoggerConsole();*/
     }
 
     private static class Console {
-        private static class LoggerConsole extends Console {
+        private final BiConsumer<Level, String> recordConsumer;
+
+        private Console(BiConsumer<Level, String> recordConsumer) {
+            this.recordConsumer = recordConsumer;
         }
+
+        /*private static class LoggerConsole extends Console {
+            public final Logger logger;
+
+            private LoggerConsole() {
+                this(LoggerConsole.class.getSimpleName());
+            }
+
+            private LoggerConsole(String name) {
+                this(Logger.getLogger(name));
+            }
+
+            private LoggerConsole(Logger logger) {
+                super(logger::log);
+                this.logger = logger;
+            }
+        }*/
     }
 }
