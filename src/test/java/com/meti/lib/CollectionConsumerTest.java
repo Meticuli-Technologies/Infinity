115878dde6cc395e251f4b09972738c3496f06f4
SirMathhman <minecraftmathhman@gmail.com>
2019-04-05 15:13:14 -0400

    Add CollectionConsumer

---
+++ src/test/java/com/meti/lib/CollectionConsumerTest.java
@@ -0,0 +1,18 @@
+package com.meti.lib;
+
+import java.util.function.Consumer;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/5/2019
+ */
+public class CollectionConsumerTest {
+    private class CollectionConsumer<T> implements Consumer<T> {
+
+        @Override
+        public void accept(T t) {
+
+        }
+    }
+}
