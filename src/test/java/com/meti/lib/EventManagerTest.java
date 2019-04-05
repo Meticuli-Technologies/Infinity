211fb4c1d4f46d869e62e9a601a5c940288f5ff7
SirMathhman <minecraftmathhman@gmail.com>
2019-04-05 15:24:31 -0400

    Implement EventManagerTest

--- src/test/java/com/meti/lib/EventManagerTest.java
+++ src/test/java/com/meti/lib/EventManagerTest.java
@@ -2,8 +2,12 @@ package com.meti.lib;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.function.Consumer;
+
+import static com.meti.lib.EventManagerTest.TestKey.TEST_KEY;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author SirMathhman
@@ -12,30 +16,56 @@ import java.util.function.Consumer;
  */
 class EventManagerTest {
     @Test
-    void compound(){
+    void compoundNoPrevious() {
+        EventManager<TestKey, TestEvent, CollectionConsumer<TestEvent>> eventManager = new EventManager<>();
 
+        ArrayList<TestEvent> list0 = new ArrayList<>();
+        CollectionConsumer<TestEvent> consumer0 = new CollectionConsumer<>(list0);
+        assertNull(eventManager.compound(TEST_KEY, consumer0));
+
+        ArrayList<TestEvent> list1 = new ArrayList<>();
+        CollectionConsumer<TestEvent> consumer1 = new CollectionConsumer<>(list1);
+        assertEquals(consumer0, eventManager.compound(TEST_KEY, consumer1));
     }
 
     @Test
     void fire(){
+        EventManager<TestKey, TestEvent, CollectionConsumer<TestEvent>> eventManager = new EventManager<>();
 
+        ArrayList<TestEvent> list = new ArrayList<>();
+        CollectionConsumer<TestEvent> consumer = new CollectionConsumer<>(list);
+        eventManager.compound(TEST_KEY, consumer);
+
+        TestEvent event = new TestEvent("test");
+        boolean wasConsumer = eventManager.fire(TEST_KEY, event);
+
+        assertTrue(wasConsumer);
+        assertEquals(1, list.size());
+        assertTrue(list.contains(event));
     }
 
-    private enum TestKey {
-        TEST_0,
-        TEST_1
+    enum TestKey {
+        TEST_KEY
     }
 
-    private class TestEvent {
+    private static class EventManager<K extends Enum<?>, E extends EventManager.Event, C extends Consumer<E>> extends HashMap<K, C> {
+        public C compound(K key, C consumer) {
+            return null;
+        }
+
+        public boolean fire(K test0, E event) {
+            return false;
+        }
+
+        private interface Event {
+        }
+    }
+
+    private class TestEvent implements EventManager.Event {
         private final String value;
 
         private TestEvent(String value) {
             this.value = value;
-        }
-    }
-
-    private static class EventManager<K extends Enum<?>, E extends EventManager.Event, C extends Consumer<E>> extends HashMap<K, C> {
-        private interface Event {
         }
     }
 }
