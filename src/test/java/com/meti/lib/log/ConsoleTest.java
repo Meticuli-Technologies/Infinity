8762c4fd077518a6eacc6c36efbe5b15000e6496
SirMathhman <minecraftmathhman@gmail.com>
2019-04-05 15:39:03 -0400

    Add LoggerConsole

--- src/test/java/com/meti/lib/log/ConsoleTest.java
+++ src/test/java/com/meti/lib/log/ConsoleTest.java
@@ -1,12 +1,20 @@
 package com.meti.lib.log;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/5/2019
  */
 public class ConsoleTest {
-    private class Console {
+    @Test
+    void log() {
+        Console console = new Console.LoggerConsole();
+    }
 
+    private static class Console {
+        private static class LoggerConsole extends Console {
+        }
     }
 }
