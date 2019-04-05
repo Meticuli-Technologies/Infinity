19f006da95179f8b9098d35d05392f54dad0abfe
SirMathhman <minecraftmathhman@gmail.com>
2019-04-05 15:11:28 -0400

    Skeleton EventManagerTest

--- src/test/java/com/meti/lib/EventManagerTest.java
+++ src/test/java/com/meti/lib/EventManagerTest.java
@@ -1,12 +1,28 @@
 package com.meti.lib;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.function.Consumer;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/5/2019
  */
-public class EventManagerTest {
-    private class EventManager {
+class EventManagerTest {
+    @Test
+    void compound(){
 
+    }
+
+    @Test
+    void fire(){
+
+    }
+
+    private static class EventManager<K extends Enum<?>, E extends EventManager.Event, C extends Consumer<E>> extends HashMap<K, C> {
+        private interface Event {
+        }
     }
 }
