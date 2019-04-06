f40776bfe714536e9cc57d1fa5c5a001424db844
SirMathhman <minecraftmathhman@gmail.com>
2019-04-06 10:56:26 -0400

    Add StageManager

---
+++ src/test/java/com/meti/lib/manage/StageManagerTest.java
@@ -0,0 +1,26 @@
+package com.meti.lib.manage;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/6/2019
+ */
+class StageManagerTest {
+    @Test
+    void allocate(){
+        assertNotNull(new StageManager().allocate());
+    }
+
+    private class StageManager extends Manager<Stage> {
+        @Override
+        public Stage allocate() {
+            return null;
+        }
+    }
+}
