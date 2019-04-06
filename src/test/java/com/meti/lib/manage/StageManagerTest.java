6292a105712fdbfa9b3426e23172de6d802aea1b
SirMathhman <minecraftmathhman@gmail.com>
2019-04-06 11:04:29 -0400

    Fix StageManagerTest

--- src/test/java/com/meti/lib/manage/StageManagerTest.java
+++ src/test/java/com/meti/lib/manage/StageManagerTest.java
@@ -3,21 +3,24 @@ package com.meti.lib.manage;
 import com.meti.lib.fx.FXUtil;
 import javafx.application.Platform;
 import javafx.stage.Stage;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/6/2019
  */
+@ExtendWith(ApplicationExtension.class)
 class StageManagerTest {
     @Test
     void allocate(){
-        assertDoesNotThrow(() -> assertNotNull(new StageManager().allocate()));
+        Platform.runLater(() -> assertDoesNotThrow(() ->
+                assertNotNull(new StageManager().allocate())));
     }
 
     private class StageManager extends Manager<Stage> {
