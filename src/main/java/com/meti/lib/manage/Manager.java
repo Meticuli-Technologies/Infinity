790bf256797cd7454aa34b771b70dad1b68badb3
SirMathhman <minecraftmathhman@gmail.com>
2019-04-06 10:55:07 -0400

    Add Manager

---
+++ src/main/java/com/meti/lib/manage/Manager.java
@@ -0,0 +1,12 @@
+package com.meti.lib.manage;
+
+import java.util.ArrayList;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/6/2019
+ */
+public abstract class Manager<T> extends ArrayList<T> {
+    public abstract T allocate();
+}
