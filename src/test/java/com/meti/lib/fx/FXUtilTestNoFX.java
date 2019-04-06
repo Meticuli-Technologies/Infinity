a9615619bd934e6a5c741eab7afefd131ac37a74
SirMathhman <minecraftmathhman@gmail.com>
2019-04-06 10:59:01 -0400

    Rename and implement

--- src/test/java/com/meti/lib/fx/FXUtilTestNoFX.java
+++ src/test/java/com/meti/lib/fx/FXUtilTestNoFX.java
@@ -2,16 +2,17 @@ package com.meti.lib.fx;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/6/2019
  */
-class FXUtilTest {
+class FXUtilTestNoFX {
 
     @Test
     void throwIfNotFX() {
+        assertThrows(IllegalStateException.class, FXUtil::throwIfNotFX);
     }
 }
