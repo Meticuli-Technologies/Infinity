aed387da16d436a228d6dfaf90e171872ee16704
SirMathhman <minecraftmathhman@gmail.com>
2019-04-06 10:44:06 -0400

    Add CompletableConsumer

---
+++ src/test/java/com/meti/lib/consume/CompletableConsumerTest.java
@@ -0,0 +1,14 @@
+package com.meti.lib.consume;
+
+import java.util.function.Consumer;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/6/2019
+ */
+public class CompletableConsumerTest {
+    private class CompletableConsumer {
+
+    }
+}
