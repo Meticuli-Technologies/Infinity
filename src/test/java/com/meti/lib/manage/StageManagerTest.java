99ced16cf65d05401d8a25843fdee3240b8a66e5
SirMathhman <minecraftmathhman@gmail.com>
2019-04-06 11:02:58 -0400

    Add exception throwing

--- src/test/java/com/meti/lib/manage/StageManagerTest.java
+++ src/test/java/com/meti/lib/manage/StageManagerTest.java
@@ -1,9 +1,11 @@
 package com.meti.lib.manage;
 
+import com.meti.lib.fx.FXUtil;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,12 +17,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class StageManagerTest {
     @Test
     void allocate(){
-        assertNotNull(new StageManager().allocate());
+        assertDoesNotThrow(() -> assertNotNull(new StageManager().allocate()));
     }
 
     private class StageManager extends Manager<Stage> {
         @Override
         public Stage allocate() {
+            FXUtil.throwIfNotFX();
             return new Stage();
         }
     }
