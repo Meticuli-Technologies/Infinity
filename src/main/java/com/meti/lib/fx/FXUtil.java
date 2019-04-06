058b600c3b6a69e55dc39f9f454e4352422c50f6
SirMathhman <minecraftmathhman@gmail.com>
2019-04-06 10:58:30 -0400

    Add FXUtil

---
+++ src/main/java/com/meti/lib/fx/FXUtil.java
@@ -0,0 +1,15 @@
+package com.meti.lib.fx;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/6/2019
+ */
+public class FXUtil {
+    private FXUtil(){
+    }
+
+    public static void throwIfNotFX(){
+
+    }
+}
---
+++ src/test/java/com/meti/lib/fx/FXUtilTest.java
@@ -0,0 +1,17 @@
+package com.meti.lib.fx;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/6/2019
+ */
+class FXUtilTest {
+
+    @Test
+    void throwIfNotFX() {
+    }
+}
