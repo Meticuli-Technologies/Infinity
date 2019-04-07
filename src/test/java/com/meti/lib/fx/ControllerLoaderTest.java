b813e42d743d43a1dada0963fb53a7f7f0e7f4d3
SirMathhman <minecraftmathhman@gmail.com>
2019-04-07 18:28:20 -0400

    Add ControllerCallbackTest

--- src/test/java/com/meti/lib/fx/ControllerLoaderTest.java
+++ src/test/java/com/meti/lib/fx/ControllerLoaderTest.java
@@ -2,7 +2,6 @@ package com.meti.lib.fx;
 
 import com.meti.lib.collect.State;
 import javafx.fxml.FXMLLoader;
-import javafx.util.Callback;
 
 /**
  * @author SirMathhman
@@ -19,25 +18,6 @@ public class ControllerLoaderTest {
             setControllerFactory(new ControllerCallback(state));
         }
 
-        private class ControllerCallback implements Callback<Class<?>, Object> {
-            private final State state;
-
-            ControllerCallback(State state) {
-                this.state = state;
-            }
-
-            @Override
-            public Object call(Class<?> param) {
-                if (Controller.class.isAssignableFrom(param)) {
-                    try {
-                        return param.getConstructor(State.class).newInstance(state);
-                    } catch (Exception e) {
-                        return null;
-                    }
-                } else {
-                    throw new IllegalArgumentException(param + " does not extend " + Controller.class);
-                }
-            }
-        }
     }
+
 }
