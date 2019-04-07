87fa5c4089e20345048b693aee66e43321031c31
SirMathhman <minecraftmathhman@gmail.com>
2019-04-07 18:36:10 -0400

    Extract ControllerLoader

---
+++ src/main/java/com/meti/lib/fx/ControllerLoader.java
@@ -0,0 +1,25 @@
+package com.meti.lib.fx;
+
+import com.meti.lib.collect.State;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/7/2019
+ */
+class ControllerLoader extends FXMLLoader {
+    final State state;
+
+    public ControllerLoader(State state) {
+        this.state = state;
+        setControllerFactory(new ControllerCallback(state));
+    }
+
+    public static <T> T load(State state, InputStream inputStream) throws IOException {
+        return new ControllerLoader(state).load(inputStream);
+    }
+}
--- src/test/java/com/meti/lib/fx/ControllerLoaderTest.java
+++ src/test/java/com/meti/lib/fx/ControllerLoaderTest.java
@@ -1,23 +1,46 @@
 package com.meti.lib.fx;
 
 import com.meti.lib.collect.State;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/7/2019
  */
-public class ControllerLoaderTest {
+class ControllerLoaderTest {
 
-    private class ControllerLoader extends FXMLLoader {
-        private final State state;
+    @Test
+    void construct() {
+        State state = new State();
+        ControllerLoader loader = new ControllerLoader(state);
 
-        public ControllerLoader(State state) {
-            this.state = state;
-            setControllerFactory(new ControllerCallback(state));
-        }
+        assertEquals(state, loader.state);
+        assertEquals(ControllerCallback.class, loader.getControllerFactory().getClass());
+    }
 
+    @Test
+    void load() throws IOException {
+        State state = new State();
+        String sampleFXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<?import javafx.scene.layout.AnchorPane?>\n" +
+                "<AnchorPane xmlns=\"http://javafx.com/javafx\"\n" +
+                "            xmlns:fx=\"http://javafx.com/fxml\"\n" +
+                "            prefHeight=\"400.0\" prefWidth=\"600.0\">\n" +
+                "</AnchorPane>\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(sampleFXML.getBytes());
+        Object token = ControllerLoader.load(state, inputStream);
+        assertEquals(AnchorPane.class, token.getClass());
+
+        AnchorPane pane = (AnchorPane) token;
+        assertEquals(600, pane.getPrefWidth());
+        assertEquals(400, pane.getPrefHeight());
     }
 
 }
