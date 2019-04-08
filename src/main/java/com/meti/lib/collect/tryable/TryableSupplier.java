ff9d97e98bd12b3b34275233c124fe437b3e72a7
SirMathhman <sirmathhman@gmail.com>
2019-04-08 13:38:49 -0400

    Remove implementation

--- src/main/java/com/meti/lib/collect/tryable/TryableSupplier.java
+++ src/main/java/com/meti/lib/collect/tryable/TryableSupplier.java
@@ -1,17 +1,5 @@
 package com.meti.lib.collect.tryable;
 
-import java.util.Optional;
-
 public interface TryableSupplier<T> {
-    Optional<T> get();
-
-    default T next(){
-        Optional<T> optional;
-        do {
-            optional = get();
-        }
-        while(!optional.isPresent());
-
-        return optional.get();
-    }
+    T get() throws Exception;
 }
