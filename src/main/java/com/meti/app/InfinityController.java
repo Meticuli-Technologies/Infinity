bccb387dfac19b1242ce44395b253945aff53b10
SirMathhman <sirmathhman@gmail.com>
2019-04-08 14:15:10 -0400

    Add InfinityController

---
+++ src/main/java/com/meti/app/InfinityController.java
@@ -0,0 +1,18 @@
+package com.meti.app;
+
+import com.meti.lib.collect.State;
+import com.meti.lib.collect.tryable.TryableFactory;
+import com.meti.lib.fx.Controller;
+import com.meti.lib.log.LoggerConsole;
+
+public class InfinityController extends Controller {
+    private final LoggerConsole console;
+    private final TryableFactory factory;
+
+    public InfinityController(State state) {
+        super(state);
+
+        this.console = state.byClass(LoggerConsole.class).findAny().orElseThrow();
+        this.factory = state.byClass(TryableFactory.class).findAny().orElseThrow();
+    }
+}
