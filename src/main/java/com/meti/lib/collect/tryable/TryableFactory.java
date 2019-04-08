9c64460f85689358e1af9130421e903a22f603c8
SirMathhman <sirmathhman@gmail.com>
2019-04-08 13:52:00 -0400

    Add default TryableFactory

--- src/main/java/com/meti/lib/collect/tryable/TryableFactory.java
+++ src/main/java/com/meti/lib/collect/tryable/TryableFactory.java
@@ -1,11 +1,14 @@
 package com.meti.lib.collect.tryable;
 
 import com.meti.lib.collect.catches.Catcher;
+import com.meti.lib.collect.catches.CollectionCatcher;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.function.Supplier;
 
 class TryableFactory {
+    public static final TryableFactory DEFAULT = new TryableFactory(new CollectionCatcher<>(new ArrayList<>()));
     Catcher catcher;
 
     public TryableFactory(Catcher catcher) {
