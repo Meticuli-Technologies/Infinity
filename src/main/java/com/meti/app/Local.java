c23abc3fa12803e149f55cf374086698bfe39dee
SirMathhman <sirmathhman@gmail.com>
2019-04-10 13:49:06 -0400

    Skeleton Local

--- src/main/java/com/meti/app/Local.java
+++ src/main/java/com/meti/app/Local.java
@@ -1,4 +1,20 @@
 package com.meti.app;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class Local {
+    @FXML
+    private TextField portField;
+
+    @FXML
+    public void back(){
+
+    }
+
+    @FXML
+    public void next(){
+
+    }
 }
