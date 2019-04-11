f3b82bda0a79d95f47fe13e46b65d5e233ad0fa5
SirMathhman <minecraftmathhman@gmail.com>
2019-04-11 18:14:41 -0400

    Add helper Controllers

---
+++ src/main/java/com/meti/InfinityController.java
@@ -0,0 +1,12 @@
+package com.meti;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/11/2019
+ */
+public class InfinityController extends Controller {
+    public InfinityController(State state) {
+        super(state);
+    }
+}
