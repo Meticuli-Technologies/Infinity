0b339c47bc39f0bfa89a0954d2843a4d6cdc62d9
SirMathhman <minecraftmathhman@gmail.com>
2019-04-11 17:24:29 -0400

    Add Main.main

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,9 +1,20 @@
 package com.meti;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/11/2019
  */
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+     launch(args);
+    }
 }
