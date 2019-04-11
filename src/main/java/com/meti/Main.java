b6ffa267851139c31868336bf22352ea22a22539
SirMathhman <minecraftmathhman@gmail.com>
2019-04-11 17:25:00 -0400

    Implement Main.start

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,6 +1,8 @@
 package com.meti;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -11,7 +13,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/meti/Menu.fxml"))));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
