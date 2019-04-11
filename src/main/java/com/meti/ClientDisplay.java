e9ab6c30080c14dece472c549b090cd29b5ae23e
SirMathhman <minecraftmathhman@gmail.com>
2019-04-11 17:32:39 -0400

    Fix dimensions

--- src/main/java/com/meti/ClientDisplay.java
+++ src/main/java/com/meti/ClientDisplay.java
@@ -1,9 +1,23 @@
 package com.meti;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/11/2019
  */
 public class ClientDisplay {
+    @FXML
+    private ListView<String> chatListView;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    public void handle(){
+
+    }
 }
