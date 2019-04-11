cfe341f930dd3784ac879be720034461a54f90a2
SirMathhman <minecraftmathhman@gmail.com>
2019-04-11 17:29:54 -0400

    Implement ServerDisplay

--- src/main/java/com/meti/ServerDisplay.java
+++ src/main/java/com/meti/ServerDisplay.java
@@ -1,9 +1,17 @@
 package com.meti;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/11/2019
  */
 public class ServerDisplay {
+    @FXML
+    private ListView<String> clientListView;
+
+    @FXML
+    private ListView<String> chatListView;
 }
