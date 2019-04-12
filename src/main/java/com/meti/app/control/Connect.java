6e5547e59ad94720c30fd25b0417092e4340c8ab
SirMathhman <minecraftmathhman@gmail.com>
2019-04-11 20:22:53 -0400

    Implement Connect

--- src/main/java/com/meti/app/control/Connect.java
+++ src/main/java/com/meti/app/control/Connect.java
@@ -1,8 +1,14 @@
 package com.meti.app.control;
 
+import com.meti.lib.net.Client;
+import com.meti.lib.net.Querier;
 import com.meti.lib.util.State;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
 /**
  * @author SirMathhman
@@ -22,11 +28,27 @@ public class Connect extends InfinityController {
 
     @FXML
     public void back() {
-
+        try {
+            onto(getClass().getResource("/com/meti/Menu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     public void next() {
+        try {
+            Socket socket = new Socket(InetAddress.getByName(addressField.getText()), Integer.parseInt(portField.getText()));
+            Client client = new Client(socket);
+            state.add(client);
 
+            Querier querier = new Querier(client);
+            state.add(querier);
+            service.submit(querier);
+
+            onto(getClass().getResource("/com/meti/Login.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
