ff677442462eb679064a52d931233b59d8616e5c
SirMathhman <minecraftmathhman@gmail.com>
2019-04-11 20:16:18 -0400

    Skeleton

--- src/main/java/com/meti/app/control/Menu.java
+++ src/main/java/com/meti/app/control/Menu.java
@@ -1,9 +1,25 @@
 package com.meti.app.control;
 
+import javafx.fxml.FXML;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/11/2019
  */
 public class Menu {
+    @FXML
+    public void connect(){
+
+    }
+
+    @FXML
+    public void host(){
+
+    }
+
+    @FXML
+    public void local(){
+
+    }
 }
