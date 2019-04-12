2c4403872d51bbca94d20540fbd8897eec5a1030
SirMathhman <minecraftmathhman@gmail.com>
2019-04-11 20:22:01 -0400

    Skeleton

--- src/main/java/com/meti/app/control/Connect.java
+++ src/main/java/com/meti/app/control/Connect.java
@@ -1,9 +1,32 @@
 package com.meti.app.control;
 
+import com.meti.lib.util.State;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/11/2019
  */
-public class Connect {
+public class Connect extends InfinityController {
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField portField;
+
+    public Connect(State state) {
+        super(state);
+    }
+
+    @FXML
+    public void back() {
+
+    }
+
+    @FXML
+    public void next() {
+
+    }
 }
