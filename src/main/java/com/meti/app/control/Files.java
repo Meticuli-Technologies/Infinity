146b09be97e4e1a8a457d1c6d6181ad4c5997d63
SirMathhman <sirmathhman@gmail.com>
2019-04-12 14:17:33 -0400

    Skeleton

--- src/main/java/com/meti/app/control/Files.java
+++ src/main/java/com/meti/app/control/Files.java
@@ -1,9 +1,26 @@
 package com.meti.app.control;
 
 import com.meti.lib.util.State;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class Files extends InfinityServerController {
+    @FXML
+    private Text nameText;
+
+    @FXML
+    private Text sizeText;
+
+    @FXML
+    private Text lastAccessedText;
+
     public Files(State state) {
         super(state);
     }
+
+    @FXML
+    public void index() {
+
+    }
 }
+
