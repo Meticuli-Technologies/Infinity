b78637bec5adc48d3c8119504b8a37ecdf723baa
SirMathhman <minecraftmathhman@gmail.com>
2019-04-23 15:56:59 -0400

    Extract initializer

--- src/main/java/com/meti/app/Infinity.java
+++ src/main/java/com/meti/app/Infinity.java
@@ -1,14 +1,10 @@
 package com.meti.app;
 
 import com.meti.app.control.Menu;
-import com.meti.lib.log.Console;
-import com.meti.lib.log.LoggerConsole;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.util.concurrent.Executors;
 import java.util.logging.Level;
 
 /**
@@ -17,28 +13,16 @@ import java.util.logging.Level;
  * @since 4/22/2019
  */
 public class Infinity implements InfinityImpl {
-    private static final Duration AWAIT_TERMINATION = Duration.ofSeconds(1);
-    private final ExecutorServiceManager executorServiceManager;
-    private final InfinityState mainState;
-    private final Console console;
-
-    Infinity() {
-        this.executorServiceManager = new ExecutorServiceManager(Executors.newCachedThreadPool(), AWAIT_TERMINATION);
-        this.mainState = new InfinityState();
-        this.console = new LoggerConsole();
-    }
+    private final InfinityInitializer infinityInitializer = new InfinityInitializer();
+    private final InfinityState infinityState = new InfinityState();
 
     @Override
     public void start(Stage primaryStage) {
-        init(primaryStage);
-        startImpl(primaryStage);
+        infinityInitializer.init(infinityState, primaryStage);
+/*        startImpl(primaryStage);*/
     }
 
-    private void init(Stage primaryStage) {
-        mainState.add(primaryStage);
-    }
-
-    private void logTaskString() {
+/*    private void logTaskString() {
         executorServiceManager.getTaskString().ifPresentOrElse(s -> console.log(Level.SEVERE, s),
                 () -> console.log(Level.INFO, "The ExecutorService has been shutdown with no tasks awaiting execution.")
         );
@@ -50,25 +34,25 @@ public class Infinity implements InfinityImpl {
         } catch (IOException e) {
             console.log(Level.SEVERE, e);
         }
-    }
+    }*/
 
     @Override
     public void stop() {
-        try {
+  /*      try {
             stopImpl();
         } catch (Exception e) {
             console.log(Level.SEVERE, e);
-        }
+        }*/
     }
 
-    private void stopImpl() throws Exception {
+/*    private void stopImpl() throws Exception {
         terminateExecutor();
     }
 
     private void terminateExecutor() throws Exception {
         logTaskString();
         executorServiceManager.checkTerminated();
-    }
+    }*/
 
     private void setAndShowScene(Stage primaryStage, Scene scene) {
         primaryStage.setScene(scene);
---
+++ src/main/java/com/meti/app/InfinityInitializer.java
@@ -0,0 +1,21 @@
+package com.meti.app;
+
+import com.meti.lib.State;
+import com.meti.lib.log.LoggerConsole;
+import javafx.stage.Stage;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+
+public class InfinityInitializer {
+    private static final Duration AWAIT_TERMINATION = Duration.ofSeconds(1);
+
+    void init(State state, Stage primaryStage) {
+        ExecutorServiceManager executorServiceManager = new ExecutorServiceManager(Executors.newCachedThreadPool(), AWAIT_TERMINATION);
+        LoggerConsole loggerConsole = new LoggerConsole();
+
+        state.add(executorServiceManager);
+        state.add(loggerConsole);
+        state.add(primaryStage);
+    }
+}
