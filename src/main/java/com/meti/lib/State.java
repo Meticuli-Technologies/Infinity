684340670eba418bb25cd19394e16aebda286a77
SirMathhman <minecraftmathhman@gmail.com>
2019-04-23 16:00:40 -0400

    Add TypePredicate and TypeFunction

--- src/main/java/com/meti/lib/State.java
+++ src/main/java/com/meti/lib/State.java
@@ -1,6 +1,10 @@
 package com.meti.lib;
 
+import com.meti.lib.collect.TypeFunction;
+import com.meti.lib.collect.TypePredicate;
+
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 /**
  * @author SirMathhman
@@ -8,4 +12,10 @@ import java.util.ArrayList;
  * @since 4/22/2019
  */
 public class State extends ArrayList<Object> {
+    public <T> Stream<T> byClass(Class<T> tClass) {
+        return stream()
+                .filter(new TypePredicate<>(tClass))
+                .map(new TypeFunction<>(tClass));
+    }
+
 }
