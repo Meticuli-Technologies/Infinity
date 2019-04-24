be38f09d4b7bb69a65e4445550f3ff5a6ab13c33
SirMathhman <sirmathhman@gmail.com>
2019-04-24 16:06:55 -0400

    Change name to parentState
    - StateController exists in a way that it must be subclassed with different types of State. However, constructors cannot confirm to that behaviour, and two fields must be set: one of type State, and one of type InfinityState, at least in this application.

--- src/main/java/com/meti/lib/fx/StateController.java
+++ src/main/java/com/meti/lib/fx/StateController.java
@@ -3,9 +3,9 @@ package com.meti.lib.fx;
 import com.meti.lib.State;
 
 public class StateController {
-    private final State state;
+    private final State parentState;
 
-    public StateController(State state){
-        this.state = state;
+    public StateController(State parentState){
+        this.parentState = parentState;
     }
 }
