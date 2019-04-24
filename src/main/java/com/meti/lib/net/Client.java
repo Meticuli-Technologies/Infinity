b2afbdf91d679b918f8cd33a7fd9b8f3957bc56a
SirMathhman <sirmathhman@gmail.com>
2019-04-24 16:19:50 -0400

    Add Client

---
+++ src/main/java/com/meti/lib/net/Client.java
@@ -0,0 +1,34 @@
+package com.meti.lib.net;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Client implements Closeable {
+    private final Socket socket;
+    private final ObjectInputStream inputStream;
+    private final ObjectOutputStream outputStream;
+
+    public Client(Socket socket) throws IOException {
+        this.socket = socket;
+        this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+        this.inputStream = new ObjectInputStream(socket.getInputStream());
+    }
+
+    @Override
+    public void close() throws IOException {
+        inputStream.close();
+        outputStream.close();
+        socket.close();
+    }
+
+    public ObjectChannel sharedChannel() {
+        return new SharedObjectChannel(inputStream, outputStream);
+    }
+
+    public ObjectChannel unsharedChannel() {
+        return new UnsharedChannel(inputStream, outputStream);
+    }
+}
