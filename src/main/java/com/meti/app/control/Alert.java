78474a6bf29473fcf0563a2efb2c3c098afc5370
SirMathhman <sirmathhman@gmail.com>
2019-04-25 14:45:34 -0400

    Implement convenience methods

--- src/main/java/com/meti/app/control/Alert.java
+++ src/main/java/com/meti/app/control/Alert.java
@@ -1,11 +1,10 @@
 package com.meti.app.control;
 
 import com.meti.lib.State;
-import com.meti.lib.fx.StateController;
+import com.meti.lib.fx.FXMLBundle;
 import com.meti.lib.fx.StateControllerLoader;
 import com.meti.lib.util.ExceptionUtil;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
@@ -13,6 +12,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.function.Consumer;
 
 public class Alert extends InfinityController {
     @FXML
@@ -27,21 +27,28 @@ public class Alert extends InfinityController {
     }
 
     public static Stage create(String message) throws IOException {
-        StateControllerLoader.loadBundle
-        Stage stage = buildStage(loader.load());
+        FXMLBundle<Alert> bundle = StateControllerLoader.loadBundle(getAlertResource(), new State());
+        bundle.initRoot(new Consumer<Parent>() {
+            @Override
+            public void accept(Parent parent) {
+
+            }
+        });
+
+/*        Stage stage = buildStage(loader.load());
         Alert controller = loader.getController();
-        controller.setMessage(message);
-        return stage;
+        controller.setMessage(message);*/
+        return new Stage();
+    }
+
+    private static URL getAlertResource() {
+        return Alert.class.getResource("/com/meti/app/control/Alert.fxml");
     }
 
     private static Stage buildStage(Parent root) {
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         return stage;
-    }
-
-    private static URL getAlertResource() {
-        return Alert.class.getResource("/com/meti/app/control/Alert.fxml");
     }
 
     private void setMessage(String message) {
--- src/main/java/com/meti/lib/fx/FXMLBundle.java
+++ src/main/java/com/meti/lib/fx/FXMLBundle.java
@@ -2,12 +2,24 @@ package com.meti.lib.fx;
 
 import javafx.scene.Parent;
 
+import java.util.function.Consumer;
+
 public class FXMLBundle<T> {
-    private final Parent parent;
+    private final Parent root;
     private final T controller;
 
-    public FXMLBundle(Parent parent, T controller) {
-        this.parent = parent;
+    public FXMLBundle(Parent root, T controller) {
+        this.root = root;
         this.controller = controller;
+    }
+
+    public FXMLBundle<T> initController(Consumer<T> controllerConsumer) {
+        controllerConsumer.accept(controller);
+        return this;
+    }
+
+    public FXMLBundle<T> initRoot(Consumer<Parent> rootConsumer) {
+        rootConsumer.accept(root);
+        return this;
     }
 }
