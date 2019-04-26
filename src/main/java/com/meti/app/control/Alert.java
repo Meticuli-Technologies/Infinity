5f0fad294c6898481cd184361ae743255482a0bd
SirMathhman <minecraftmathhman@gmail.com>
2019-04-26 15:56:13 -0400

    Fix Alert

--- src/main/java/com/meti/app/control/Alert.java
+++ src/main/java/com/meti/app/control/Alert.java
@@ -6,6 +6,8 @@ import com.meti.lib.concurrent.CompletableConsumer;
 import com.meti.lib.fx.FXMLBundle;
 import com.meti.lib.util.ExceptionUtil;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -19,20 +21,19 @@ public class Alert extends InfinityController {
     @FXML
     private Text messageText;
 
+    @FXML
+    private TextArea stackTraceArea;
+
     public Alert(State state) {
         super(state);
     }
 
     public static Optional<Stage> create(Exception exception, InfinityState state) {
-        return create(ExceptionUtil.stackTraceString(exception), state);
-    }
-
-    public static Optional<Stage> create(String message, InfinityState state) {
         try {
             CompletableConsumer<Stage> consumer = new CompletableConsumer<>();
             FXMLBundle<Alert> bundle = loadBundle(getAlertResource(), state);
             bundle.initRoot(new StageBuilder(consumer))
-                    .initController(alert -> alert.setMessage(message));
+                    .initController(alert -> alert.show(exception));
             return Optional.of(consumer.get());
         } catch (Exception e) {
             state.getConsole().log(Level.WARNING, e);
@@ -40,11 +41,12 @@ public class Alert extends InfinityController {
         }
     }
 
-    private static URL getAlertResource() {
-        return Alert.class.getResource("/com/meti/app/control/Alert.fxml");
+    private void show(Exception exception) {
+        messageText.setText(exception.getMessage());
+        stackTraceArea.setText(ExceptionUtil.stackTraceString(exception));
     }
 
-    private void setMessage(String message) {
-        messageText.setText(message);
+    private static URL getAlertResource() {
+        return Alert.class.getResource("/com/meti/app/control/Alert.fxml");
     }
 }
