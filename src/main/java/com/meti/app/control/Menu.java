09867dacddd827e2db5d55ad4e9f9337bff74892
SirMathhman <minecraftmathhman@gmail.com>
2019-04-26 17:06:44 -0400

    Implement Menu

--- src/main/java/com/meti/app/control/Menu.java
+++ src/main/java/com/meti/app/control/Menu.java
@@ -1,23 +1,19 @@
 package com.meti.app.control;
 
-import com.meti.app.io.InfinityServerListener;
 import com.meti.lib.State;
 import com.meti.lib.fx.StateControllerLoader;
-import com.meti.lib.net.Client;
-import com.meti.lib.net.Querier;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.net.URL;
 
 import static com.meti.lib.util.URLUtil.getResource;
 
 public class Menu extends InfinityController {
+    private final MenuModel menuModel = new MenuModel(state);
     @FXML
     private TextField portField;
 
@@ -41,29 +37,9 @@ public class Menu extends InfinityController {
     @FXML
     public void next() {
         try{
-            nextImpl();
+            menuModel.nextImpl(Integer.parseInt(portField.getText()));
         } catch (Exception e){
             Alerts.showInstance(e, state);
         }
-    }
-
-    private void nextImpl() throws IOException {
-        int port = Integer.parseInt(portField.getText());
-        constructServerListener(port);
-        constructClient(port);
-    }
-
-    private void constructServerListener(int port) throws IOException {
-        InfinityServerListener serverListener = new InfinityServerListener(port, state.getExecutorServiceManager().service);
-        serverListener.listen();
-        state.add(serverListener);
-    }
-
-    private void constructClient(int port) throws IOException {
-        Client client = new Client(new Socket(InetAddress.getByName("localhost"), port));
-        state.add(client);
-        Querier querier = new Querier(true, client);
-        state.getExecutorServiceManager().service.submit(querier);
-        state.add(querier);
     }
 }
---
+++ src/main/java/com/meti/app/control/MenuModel.java
@@ -0,0 +1,42 @@
+package com.meti.app.control;
+
+import com.meti.app.core.runtime.InfinityState;
+import com.meti.app.io.InfinityServerListener;
+import com.meti.lib.net.Client;
+import com.meti.lib.net.Querier;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 4/26/2019
+ */
+public class MenuModel {
+    private final InfinityState state;
+
+    public MenuModel(InfinityState state) {
+        this.state = state;
+    }
+
+    void nextImpl(int port) throws IOException {
+        constructServerListener(port);
+        constructClient(port);
+    }
+
+    private void constructServerListener(int port) throws IOException {
+        InfinityServerListener serverListener = new InfinityServerListener(port, state.getExecutorServiceManager().service);
+        serverListener.listen();
+        state.add(serverListener);
+    }
+
+    private void constructClient(int port) throws IOException {
+        Client client = new Client(new Socket(InetAddress.getByName("localhost"), port));
+        state.add(client);
+        Querier querier = new Querier(true, client);
+        state.getExecutorServiceManager().service.submit(querier);
+        state.add(querier);
+    }
+}
