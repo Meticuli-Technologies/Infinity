34ac58a1799f0f0bb0c83ef86bad4f8555d28a22
SirMathhman <minecraftmathhman@gmail.com>
2019-04-28 12:51:38 -0400

    Pull down

--- src/main/java/com/meti/lib/net/ExecutorServiceListener.java
+++ src/main/java/com/meti/lib/net/ExecutorServiceListener.java
@@ -1,7 +1,11 @@
 package com.meti.lib.net;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author SirMathhman
@@ -10,6 +14,7 @@ import java.util.concurrent.ExecutorService;
  */
 public class ExecutorServiceListener extends ServerListener {
     private final ExecutorService service;
+    private final Map<Predicate<Object>, Function<Object, Object>> resultMapper = new HashMap<>();
 
     public ExecutorServiceListener(int port, ExecutorService service) throws IOException {
         super(new Server(port));
@@ -24,5 +29,10 @@ public class ExecutorServiceListener extends ServerListener {
     @Override
     protected void process(Processor processor) {
         service.submit(processor);
+    }
+
+    @Override
+    protected Processor createProcessor(Client client) {
+        return new MappedProcessor(client, resultMapper);
     }
 }
--- src/main/java/com/meti/lib/net/ServerListener.java
+++ src/main/java/com/meti/lib/net/ServerListener.java
@@ -1,13 +1,8 @@
 package com.meti.lib.net;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.Callable;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 public abstract class ServerListener implements Callable<Void> {
-    private final Map<Predicate<Object>, Function<Object, Object>> resultMapper = new HashMap<>();
     private final Server server;
 
     public ServerListener(Server server) {
@@ -23,9 +18,11 @@ public abstract class ServerListener implements Callable<Void> {
     }
 
     public void nextClient(Client client) {
-        process(new MappedProcessor(client, resultMapper));
+        process(createProcessor(client));
     }
 
     public abstract void listen();
+
+    protected abstract Processor createProcessor(Client client);
     protected abstract void process(Processor processor);
 }
