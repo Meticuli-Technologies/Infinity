117609614cc0b74573d9b80a9f884abeb20c8b75
SirMathhman <sirmathhman@gmail.com>
2019-04-29 14:29:36 -0400

    Skeleton

--- src/main/java/com/meti/app/control/ServerDisplay.java
+++ src/main/java/com/meti/app/control/ServerDisplay.java
@@ -1,4 +1,15 @@
 package com.meti.app.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
 public class ServerDisplay {
+    @FXML
+    private Text portText;
+
+    @FXML
+    private ListView<String> clientListView;
+
+
 }
