880a62bd4184ccb86c880871ce1760e5be47d75a
SirMathhman <minecraftmathhman@gmail.com>
2019-04-30 16:12:22 -0400

    Make extends StateControlelr

--- src/main/java/com/meti/app/control/ClientDisplay.java
+++ src/main/java/com/meti/app/control/ClientDisplay.java
@@ -1,5 +1,7 @@
 package com.meti.app.control;
 
+import com.meti.app.control.helper.InfinityClientController;
+import com.meti.lib.State;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
@@ -9,7 +11,7 @@ import javafx.scene.text.Text;
  * @version 0.0.0
  * @since 4/30/2019
  */
-public class ClientDisplay {
+public class ClientDisplay extends InfinityClientController {
     @FXML
     private Text addressText;
 
@@ -18,4 +20,8 @@ public class ClientDisplay {
 
     @FXML
     private ListView<String> viewListView;
+
+    public ClientDisplay(State state) {
+        super(state);
+    }
 }
