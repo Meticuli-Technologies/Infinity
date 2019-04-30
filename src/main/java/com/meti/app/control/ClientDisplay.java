932e2112a00a58ff035d74f74d9dd27fb9679a4c
SirMathhman <minecraftmathhman@gmail.com>
2019-04-30 16:12:01 -0400

    Add ClientDisplay

--- src/main/java/com/meti/app/control/ClientDisplay.java
+++ src/main/java/com/meti/app/control/ClientDisplay.java
@@ -1,9 +1,21 @@
 package com.meti.app.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 4/30/2019
  */
 public class ClientDisplay {
+    @FXML
+    private Text addressText;
+
+    @FXML
+    private Text portText;
+
+    @FXML
+    private ListView<String> viewListView;
 }
