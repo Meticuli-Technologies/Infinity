ecede5ee7e7728bb8dce136db2f816b5f587a90b
SirMathhman <sirmathhman@gmail.com>
2019-05-01 13:34:38 -0400

    Add Module

---
+++ src/main/java/com/meti/lib/module/Module.java
@@ -0,0 +1,4 @@
+package com.meti.lib.module;
+
+public interface Module {
+}
---
+++ src/main/java/com/meti/lib/module/ModuleManager.java
@@ -0,0 +1,8 @@
+package com.meti.lib.module;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModuleManager {
+    private final Map<String, Module> modules = new HashMap<>();
+}
