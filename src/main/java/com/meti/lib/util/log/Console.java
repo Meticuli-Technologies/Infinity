a9b2704f468051b90997ba185a9de43e1646462d
SirMathhman <sirmathhman@gmail.com>
2019-05-01 14:14:02 -0400

    Add TryableFactory to Console

--- src/main/java/com/meti/lib/util/log/Console.java
+++ src/main/java/com/meti/lib/util/log/Console.java
@@ -1,5 +1,7 @@
 package com.meti.lib.util.log;
 
+import com.meti.lib.util.tryable.TryableFactory;
+
 import java.util.function.BiConsumer;
 import java.util.logging.Level;
 
@@ -11,16 +13,37 @@ import java.util.logging.Level;
 public abstract class Console implements BiConsumer<Level, String> {
     private final LogBuilder logBuilder = new LogBuilder();
 
+    public TryableFactory constructFactory() {
+        return constructFactory(Level.WARNING);
+    }
+
+    public TryableFactory constructFactory(Level level) {
+        return new ConsoleTryableFactory(level);
+    }
+
     public void log(Level level, Exception exception) {
         log(level, null, exception);
+    }
+
+    private void log(Level level, String message, Exception exception) {
+        String result = logBuilder.log(message, exception);
+        accept(level, result);
     }
 
     public void log(Level level, String message) {
         log(level, message, null);
     }
 
-    private void log(Level level, String message, Exception exception){
-        String result = logBuilder.log(message, exception);
-        accept(level, result);
+    private class ConsoleTryableFactory extends TryableFactory {
+        private final Level level;
+
+        public ConsoleTryableFactory(Level level) {
+            this.level = level;
+        }
+
+        @Override
+        public void handle(Exception e) {
+            log(level, e);
+        }
     }
 }
