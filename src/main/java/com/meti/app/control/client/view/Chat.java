f10288cf9ea0be6e7a7835a08cf48f9bf60271be
SirMathhman <sirmathhman@gmail.com>
2019-05-02 11:00:23 -0400

    Skeleton

--- src/main/java/com/meti/app/control/client/view/Chat.java
+++ src/main/java/com/meti/app/control/client/view/Chat.java
@@ -2,8 +2,17 @@ package com.meti.app.control.client.view;
 
 import com.meti.app.control.client.InfinityClientController;
 import com.meti.lib.util.collect.State;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Chat extends InfinityClientController {
+    @FXML
+    private TextField input;
+
+    @FXML
+    private TextArea output;
+
     public Chat(State state) {
         super(state);
     }
