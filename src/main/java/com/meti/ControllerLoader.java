b908ec01d985e8ba0e64ac424219121b171282a8
SirMathhman <sirmathhman@gmail.com>
2019-05-09 13:58:35 -0400

    Add ControllerLoader

---
+++ src/main/java/com/meti/ControllerLoader.java
@@ -0,0 +1,37 @@
+package com.meti;
+
+import javafx.fxml.FXMLLoader;
+import javafx.util.Callback;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class ControllerLoader extends FXMLLoader {
+    private final List<Object> injectableList = new ArrayList<>();
+
+    public ControllerLoader(Object... injectableArray) {
+        this.injectableList.addAll(Arrays.asList(injectableArray));
+        setControllerFactory(new ControllerFactory());
+    }
+
+    private class ControllerFactory implements Callback<Class<?>, Object> {
+        @Override
+        public Object call(Class<?> param) {
+            try {
+                Class[] classes = getClassArray(injectableList);
+                return param.getDeclaredConstructor(classes)
+                        .newInstance(injectableList);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public Class[] getClassArray(List<Object> objects) {
+            return objects.stream()
+                    .map((Function<Object, Class<?>>) Object::getClass)
+                    .toArray(Class[]::new);
+        }
+    }
+}
