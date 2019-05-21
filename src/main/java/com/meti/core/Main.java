0e310730f1a6b309b997b287aa3b270b994895c2
SirMathhman <minecraftmathhman@gmail.com>
2019-05-21 19:51:07 -0400

    Add Infinity

---
+++ src/main/java/com/meti/core/Main.java
@@ -0,0 +1,27 @@
+package com.meti.core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 5/21/2019
+ */
+public class Main extends Application  {
+    private final Infinity infinity = new Infinity();
+
+    @Override
+    public void start(Stage primaryStage) {
+        infinity.start(primaryStage);
+    }
+
+    @Override
+    public void stop() {
+        infinity.stop();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
