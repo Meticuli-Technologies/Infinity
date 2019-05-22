58e13bc3f5d14bbd0fc7d99164a1e9ada9060c29
SirMathhman <sirmathhman@gmail.com>
2019-05-22 11:08:59 -0400

    Implement Hopper

--- src/main/java/com/meti/lib/net/Hopper.java
+++ src/main/java/com/meti/lib/net/Hopper.java
@@ -1,4 +1,26 @@
 package com.meti.lib.net;
 
-public class Hopper {
+import com.meti.lib.source.ReadableSource;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.concurrent.Callable;
+
+public class Hopper implements Callable<ReadableSource<ObjectInputStream>> {
+    private final ReadableSource<ObjectInputStream> readable;
+
+    public Hopper(ReadableSource<ObjectInputStream> readable) {
+        this.readable = readable;
+    }
+
+    @Override
+    public ReadableSource<ObjectInputStream> call() throws IOException, ClassNotFoundException {
+        try (ObjectInputStream inputStream = readable.getInputStream()) {
+            while (readable.isOpen()) {
+                Object token = inputStream.readObject();
+                //TODO: handle token
+            }
+        }
+        return readable;
+    }
 }
