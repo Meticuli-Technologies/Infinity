94c710de14ddf5c31b0e21eace5ca249c9317d46
SirMathhman <minecraftmathhman@gmail.com>
2019-05-21 20:17:34 -0400

    Add Logger

--- src/main/java/com/meti/core/Infinity.java
+++ src/main/java/com/meti/core/Infinity.java
@@ -5,16 +5,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Infinity {
     private final StateImpl stateImpl = new InfinityState();
+    private final Logger logger = Logger.getLogger("Infinity");
 
     public void start(Stage primaryStage) {
         try {
             Properties properties = new PropertiesLoader().load();
             stateImpl.add(properties);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Failed to start Infinity", e);
         }
     }
 
