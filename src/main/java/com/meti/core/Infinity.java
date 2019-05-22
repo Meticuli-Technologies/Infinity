d77a624c49fc7eea301c2c44589752a888f08594
SirMathhman <minecraftmathhman@gmail.com>
2019-05-21 20:02:01 -0400

    Add properties into Infinity

--- src/main/java/com/meti/core/Infinity.java
+++ src/main/java/com/meti/core/Infinity.java
@@ -2,12 +2,31 @@ package com.meti.core;
 
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class Infinity {
     public Infinity() {
     }
 
     public void start(Stage primaryStage) {
+        try {
+            Path propertiesPath = Paths.get(".\\Infinity.properties");
+            if(!Files.exists(propertiesPath)){
+                Files.createFile(propertiesPath);
+            }
 
+            InputStream inStream = Files.newInputStream(propertiesPath);
+            Properties properties = new Properties();
+            properties.load(inStream);
+            inStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void stop() {
