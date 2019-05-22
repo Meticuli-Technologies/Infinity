c2f7cd173c9711fb91fc4ef0e07a9ba258c0a8be
SirMathhman <minecraftmathhman@gmail.com>
2019-05-21 20:24:15 -0400

    Add ModuleBuilder

---
+++ src/main/java/com/meti/lib/module/ModuleBuilder.java
@@ -0,0 +1,9 @@
+package com.meti.lib.module;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 5/21/2019
+ */
+public interface ModuleBuilder {
+}
--- src/main/java/com/meti/lib/module/ModuleManager.java
+++ src/main/java/com/meti/lib/module/ModuleManager.java
@@ -1,9 +1,14 @@
 package com.meti.lib.module;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 5/21/2019
  */
 public class ModuleManager {
+    private final Set<ModuleBuilder> builders = new HashSet<>();
+
 }
