f436d6e1834d08d5a02d34011e914e97fd3bd980
SirMathhman <minecraftmathhman@gmail.com>
2019-05-30 12:31:14 -0400

    Skeleton ClientMain

--- src/main/java/com/meti/ClientMain.java
+++ src/main/java/com/meti/ClientMain.java
@@ -6,4 +6,39 @@ package com.meti;
  * @since 5/30/2019
  */
 public class ClientMain {
+    public static void main(String[] args) {
+        ClientMain clientMain = new ClientMain();
+        clientMain.init();
+        clientMain.start();
+        clientMain.run();
+        clientMain.stop();
+    }
+
+    private void init() {
+
+    }
+
+    private void start() {
+
+    }
+
+    private void run() {
+        while (shouldContinue()) {
+            loop();
+        }
+
+        stop();
+    }
+
+    private void stop() {
+
+    }
+
+    private boolean shouldContinue() {
+        return false;
+    }
+
+    private void loop() {
+
+    }
 }
