35ecaea457528d0039ef54572bd2e3095ac55e68
SirMathhman <minecraftmathhman@gmail.com>
2019-05-30 12:24:50 -0400

    Add sockets

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,10 +1,14 @@
 package com.meti;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static java.net.InetAddress.*;
 
 /**
  * @author SirMathhman
@@ -15,6 +19,8 @@ public class Main {
     private final Logger logger = Logger.getLogger("Infinity");
     private final Scanner scanner = new Scanner(System.in);
     private ServerSocket serverSocket;
+    private Socket acceptedSocket;
+    private Socket clientSocket;
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -26,13 +32,18 @@ public class Main {
     private void init() {
         try {
             this.serverSocket = new ServerSocket(0);
+            this.clientSocket = new Socket(getByName("localhost"), serverSocket.getLocalPort());
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Failed to initialize Infinity", e);
         }
     }
 
     private void start() {
-
+        try {
+            this.acceptedSocket = serverSocket.accept();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to start Infinity", e);
+        }
     }
 
     private void run() {
