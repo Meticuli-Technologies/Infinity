901b5104182fe12c76e00566c68895c29971f728
SirMathhman <minecraftmathhman@gmail.com>
2019-05-30 12:20:18 -0400

    Add skeleton

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -6,4 +6,38 @@ package com.meti;
  * @since 5/30/2019
  */
 public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.init();
+        main.start();
+        main.run();
+    }
+
+    private void init() {
+
+    }
+
+    private void start() {
+
+    }
+
+    private void run() {
+        while (shouldContinue()) {
+            loop();
+        }
+
+        stop();
+    }
+
+    private boolean shouldContinue() {
+        return false;
+    }
+
+    private void loop() {
+
+    }
+
+    private void stop() {
+
+    }
 }
