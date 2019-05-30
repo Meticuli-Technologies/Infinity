341665b1598e685274ebb9e1e4cab91853c7a45f
SirMathhman <minecraftmathhman@gmail.com>
2019-05-30 12:43:32 -0400

    Add ServerMain

---
+++ src/main/java/com/meti/ServerMain.java
@@ -0,0 +1,12 @@
+package com.meti;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 5/30/2019
+ */
+public class ServerMain {
+    public static void main(String[] args) {
+        ServerMain serverMain = new ServerMain();
+    }
+}
