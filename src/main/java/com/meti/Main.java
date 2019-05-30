de17f95a4a2f083fb6d3ba07bdeac0910d710a64
SirMathhman <minecraftmathhman@gmail.com>
2019-05-30 12:23:16 -0400

    Add ServerSocket

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,11 +1,21 @@
 package com.meti;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 5/30/2019
  */
 public class Main {
+    private final Logger logger = Logger.getLogger("Infinity");
+    private final Scanner scanner = new Scanner(System.in);
+    private ServerSocket serverSocket;
+
     public static void main(String[] args) {
         Main main = new Main();
         main.init();
@@ -14,7 +24,11 @@ public class Main {
     }
 
     private void init() {
-
+        try {
+            this.serverSocket = new ServerSocket(0);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to initialize Infinity", e);
+        }
     }
 
     private void start() {
@@ -38,6 +52,10 @@ public class Main {
     }
 
     private void stop() {
-
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to stop Infinity", e);
+        }
     }
 }
