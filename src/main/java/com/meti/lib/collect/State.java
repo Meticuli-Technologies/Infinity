050d3d7e2d329de092ffbf8d275a978b488bc43b
SirMathhman <minecraftmathhman@gmail.com>
2019-06-01 15:42:35 -0400

    Add components

--- src/main/java/com/meti/lib/collect/State.java
+++ src/main/java/com/meti/lib/collect/State.java
@@ -1,9 +1,13 @@
 package com.meti.lib.collect;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 6/1/2019
  */
 public class State {
+    private final Set<Object> components = new HashSet<>();
 }
