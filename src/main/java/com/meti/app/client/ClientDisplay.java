a0904ff8d901f19df51dc9210799ec09151d79b7
SirMathhman <minecraftmathhman@gmail.com>
2019-06-01 15:00:04 -0400

    Skeleton

--- src/main/java/com/meti/app/client/ClientDisplay.java
+++ src/main/java/com/meti/app/client/ClientDisplay.java
@@ -1,9 +1,18 @@
 package com.meti.app.client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 6/1/2019
  */
 public class ClientDisplay {
+    @FXML
+    private TextArea output;
+
+    @FXML
+    private TextField input;
 }
