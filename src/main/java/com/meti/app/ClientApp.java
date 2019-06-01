6af0436f9a4416d82c95441b0fa741ede6bce9e6
SirMathhman <minecraftmathhman@gmail.com>
2019-06-01 15:46:04 -0400

    Add InjectorLoader to ClientApp

--- src/main/java/com/meti/app/ClientApp.java
+++ src/main/java/com/meti/app/ClientApp.java
@@ -2,11 +2,14 @@ package com.meti.app;
 
 import com.meti.lib.collect.SetBasedState;
 import com.meti.lib.collect.State;
+import com.meti.lib.fx.InjectorLoader;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.List;
 
 /**
  * @author SirMathhman
@@ -18,9 +21,13 @@ public class ClientApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("/com/meti/app/client/ClientDisplay.fxml"));
+        Parent parent = InjectorLoader.load(List.of(state), getClientDisplayURL());
         primaryStage.setScene(new Scene(parent));
         primaryStage.show();
+    }
+
+    private URL getClientDisplayURL() {
+        return getClass().getResource("/com/meti/app/client/ClientDisplay.fxml");
     }
 
     public static void main(String[] args) {
