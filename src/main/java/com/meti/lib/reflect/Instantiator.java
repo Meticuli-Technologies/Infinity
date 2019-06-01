dbdb1dc9f3a165dc1b16e58c307c37b4337aa67f
SirMathhman <minecraftmathhman@gmail.com>
2019-06-01 15:34:21 -0400

    Clean

--- src/main/java/com/meti/lib/reflect/Instantiator.java
+++ src/main/java/com/meti/lib/reflect/Instantiator.java
@@ -18,29 +18,53 @@ public class Instantiator {
 
     public Set<Object> instantiate(Class<?> clazz) throws IllegalAccessException, InvocationTargetException, InstantiationException {
         List<Constructor<?>> constructors = List.of(clazz.getConstructors());
+        Map<List<Class<?>>, Constructor<?>> parameterTypeMap = createParameterTypeMap(constructors);
+        List<Constructor<?>> validConstructors = filterValidConstructors(parameterTypeMap);
+        return instantiate(validConstructors);
+    }
+
+    private Map<List<Class<?>>, Constructor<?>> createParameterTypeMap(Iterable<Constructor<?>> constructors) {
         Map<List<Class<?>>, Constructor<?>> parameterTypeMap = new HashMap<>();
         for (Constructor<?> constructor : constructors) {
             parameterTypeMap.put(List.of(constructor.getParameterTypes()), constructor);
         }
+        return parameterTypeMap;
+    }
+
+    private List<Constructor<?>> filterValidConstructors(Map<List<Class<?>>, Constructor<?>> parameterTypeMap) {
         List<Constructor<?>> validConstructors = new ArrayList<>();
-        for (Map.Entry<List<Class<?>>, Constructor<?>> listConstructorEntry : parameterTypeMap.entrySet()) {
-            if ((listConstructorEntry.getKey()).size() == dependencies.size()) {
-                boolean shouldAdd = true;
-                for (int i = 0; i < (listConstructorEntry.getKey()).size(); i++) {
-                    if (!(listConstructorEntry.getKey()).get(i).isInstance(dependencies.get(i))) {
-                        shouldAdd = false;
-                    }
-                }
-                if (shouldAdd) {
-                    validConstructors.add(listConstructorEntry.getValue());
-                }
+        for (Map.Entry<List<Class<?>>, Constructor<?>> parameterTypeEntry : parameterTypeMap.entrySet()) {
+            if (areValidParameters(parameterTypeEntry.getKey())) {
+                validConstructors.add(parameterTypeEntry.getValue());
             }
         }
+        return validConstructors;
+    }
+
+    private Set<Object> instantiate(Iterable<Constructor<?>> validConstructors) throws InstantiationException, IllegalAccessException, InvocationTargetException {
         Set<Object> instances = new HashSet<>();
         for (Constructor<?> validConstructor : validConstructors) {
             Object instance = validConstructor.newInstance(dependencies.toArray());
             instances.add(instance);
         }
         return instances;
+    }
+
+    private boolean areValidParameters(List<Class<?>> key) {
+        return isEqualSize(key, dependencies) && areDependenciesInstancesOf(key);
+    }
+
+    private boolean isEqualSize(Collection<?> collection0, Collection<?> collection1) {
+        return collection0.size() == collection1.size();
+    }
+
+    @SuppressWarnings("MethodWithMultipleReturnPoints")
+    private boolean areDependenciesInstancesOf(List<Class<?>> parameterTypes) {
+        for (int i = 0; i < (parameterTypes).size(); i++) {
+            if (!parameterTypes.get(i).isInstance(dependencies.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
