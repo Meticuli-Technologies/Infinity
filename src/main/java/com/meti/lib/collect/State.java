134e654b484489203e7c6d662dd580611a906b0e
SirMathhman <minecraftmathhman@gmail.com>
2019-06-01 15:44:14 -0400

    Add helper methods

--- src/main/java/com/meti/lib/collect/State.java
+++ src/main/java/com/meti/lib/collect/State.java
@@ -1,7 +1,9 @@
 package com.meti.lib.collect;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author SirMathhman
@@ -10,4 +12,19 @@ import java.util.Set;
  */
 public class State {
     private final Set<Object> components = new HashSet<>();
+
+    public boolean add(Object o) {
+        return components.add(o);
+    }
+
+    public <T> Optional<T> singleByClass(Class<T> tClass) {
+        return filterByClass(tClass).stream().findAny();
+    }
+
+    private <T> Set<T> filterByClass(Class<T> tClass) {
+        return components.stream()
+                .filter(tClass::isInstance)
+                .map(tClass::cast)
+                .collect(Collectors.toSet());
+    }
 }
