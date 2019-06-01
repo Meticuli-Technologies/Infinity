37758f0f12f0cd52fc84ff62524780d0ef593525
SirMathhman <minecraftmathhman@gmail.com>
2019-06-01 14:54:47 -0400

    Add ClientApp

---
+++ src/main/java/com/meti/app/ClientApp.java
@@ -0,0 +1,20 @@
+package com.meti.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 6/1/2019
+ */
+public class ClientApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
