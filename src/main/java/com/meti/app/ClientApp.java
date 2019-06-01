9225fcfa4c319bfc535d99147ba4c81baf8c4cbe
SirMathhman <minecraftmathhman@gmail.com>
2019-06-01 14:58:54 -0400

    Show ClientDisplay on ClientApp

--- src/main/java/com/meti/app/ClientApp.java
+++ src/main/java/com/meti/app/ClientApp.java
@@ -1,6 +1,9 @@
 package com.meti.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -11,7 +14,9 @@ import javafx.stage.Stage;
 public class ClientApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent parent = FXMLLoader.load(getClass().getResource("/com/meti/app/client/ClientDisplay.fxml"));
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
