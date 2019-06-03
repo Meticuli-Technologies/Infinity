64e9a61585a39c8d3772513cbd4a9d76304913e2
SirMathhman <minecraftmathhman@gmail.com>
2019-06-03 13:23:47 -0400

    Add Controls

---
+++ src/main/java/com/meti/app/Controls.java
@@ -0,0 +1,13 @@
+package com.meti.app;
+
+import com.meti.lib.collect.State;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 6/3/2019
+ */
+public interface Controls {
+    State getState();
+    Toolkit getToolkit();
+}
