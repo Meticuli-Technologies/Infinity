1d74a566dd1464ae7c62127f67c2e6e31f503d6f
SirMathhman <minecraftmathhman@gmail.com>
2019-06-03 13:12:26 -0400

    Implement ClientMenu

--- src/main/java/com/meti/app/client/ClientMenu.java
+++ src/main/java/com/meti/app/client/ClientMenu.java
@@ -1,7 +1,11 @@
 package com.meti.app.client;
 
+import com.meti.lib.collect.State;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author SirMathhman
@@ -15,8 +19,22 @@ public class ClientMenu {
     @FXML
     private TextField portField;
 
+    private final State state;
+
+    public ClientMenu(State state) {
+        this.state = state;
+    }
+
     @FXML
     public void connect(){
+        try {
+            InetAddress address = InetAddress.getByName(addressField.getText());
+            int port = Integer.parseInt(portField.getText());
 
+            ClientBootstrap bootstrap = new SocketClientBootstrap(address, port);
+            state.add(bootstrap);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 }
