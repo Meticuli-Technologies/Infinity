251a9095cabe6c1e64440d0cd7d1789a9f83bcab
SirMathhman <minecraftmathhman@gmail.com>
2019-06-03 13:01:55 -0400

    Skeleton

--- src/main/java/com/meti/app/client/ClientMenu.java
+++ src/main/java/com/meti/app/client/ClientMenu.java
@@ -1,9 +1,22 @@
 package com.meti.app.client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 6/3/2019
  */
 public class ClientMenu {
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField portField;
+
+    @FXML
+    public void connect(){
+
+    }
 }
