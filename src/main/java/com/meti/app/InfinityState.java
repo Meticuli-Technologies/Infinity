dd1313831ba2daace01e2122eb3777e8c3df7588
SirMathhman <minecraftmathhman@gmail.com>
2019-06-03 13:18:48 -0400

    Add StageManager to InfinityState

--- src/main/java/com/meti/app/InfinityState.java
+++ src/main/java/com/meti/app/InfinityState.java
@@ -1,14 +1,15 @@
 package com.meti.app;
 
 import com.meti.lib.collect.SetBasedState;
+import com.meti.lib.fx.StageManager;
 
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 6/3/2019
  */
-public class InfinityState extends SetBasedState {
+public final class InfinityState extends SetBasedState {
     public InfinityState() {
- /*       add();*/
+        add(new StageManager());
     }
 }
