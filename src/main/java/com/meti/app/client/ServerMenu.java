cf65ed5cc4dda78c8fd85707c31038bfb8e7fa94
SirMathhman <minecraftmathhman@gmail.com>
2019-06-03 14:32:26 -0400

    Skeleton controller

--- src/main/java/com/meti/app/client/ServerMenu.java
+++ src/main/java/com/meti/app/client/ServerMenu.java
@@ -1,9 +1,19 @@
 package com.meti.app.client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 6/3/2019
  */
 public class ServerMenu {
+    @FXML
+    private TextField portField;
+
+    @FXML
+    public void host() {
+
+    }
 }
