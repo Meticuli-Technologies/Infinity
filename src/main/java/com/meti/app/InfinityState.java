a66320c33357769b5de535712a62bf84b276cd24
SirMathhman <minecraftmathhman@gmail.com>
2019-06-03 13:14:50 -0400

    Add InfinityState

---
+++ src/main/java/com/meti/app/InfinityState.java
@@ -0,0 +1,14 @@
+package com.meti.app;
+
+import com.meti.lib.collect.SetBasedState;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 6/3/2019
+ */
+public class InfinityState extends SetBasedState {
+    public InfinityState() {
+ /*       add();*/
+    }
+}
