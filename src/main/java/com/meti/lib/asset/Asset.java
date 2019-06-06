dfee73e41aad88241a7dce7d264c3d977faec6d9
SirMathhman <minecraftmathhman@gmail.com>
2019-06-06 14:00:28 -0400

    Add parts

--- src/main/java/com/meti/lib/asset/Asset.java
+++ src/main/java/com/meti/lib/asset/Asset.java
@@ -1,10 +1,18 @@
 package com.meti.lib.asset;
 
+import java.util.function.BiConsumer;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 6/6/2019
  */
-public interface Asset {
+public interface Asset<A extends AssetComponents, C extends AssetChange> {
     String getName();
+
+    void change(C change);
+
+    A getChanges();
+
+    void onChange(BiConsumer<C, Asset<A, C>> consumer);
 }
