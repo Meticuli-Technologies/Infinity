c9e6ab4f526bc40f0d01b041d776f203fe6666db
SirMathhman <minecraftmathhman@gmail.com>
2019-06-06 12:37:06 -0400

    Skeleton

--- src/main/java/com/meti/app/client/ClientDisplay.java
+++ src/main/java/com/meti/app/client/ClientDisplay.java
@@ -1,9 +1,20 @@
 package com.meti.app.client;
 
+import javafx.fxml.FXML;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 6/6/2019
  */
-public class ClientDisplay {
+public class ClientDisplay{
+    @FXML
+    public void openChat(){
+
+    }
+
+    @FXML
+    public void openIssues(){
+
+    }
 }
