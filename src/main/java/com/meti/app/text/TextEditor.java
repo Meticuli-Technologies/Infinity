d7705410f4c0029fb7c60e73dd88b216700866b0
SirMathhman <minecraftmathhman@gmail.com>
2019-06-07 13:15:51 -0400

    Add TextEditor

---
+++ src/main/java/com/meti/app/text/TextEditor.java
@@ -0,0 +1,28 @@
+package com.meti.app.text;
+
+import com.meti.app.client.Editor;
+import com.meti.lib.asset.properties.AssetProperties;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 6/7/2019
+ */
+public class TextEditor implements Editor {
+    @Override
+    public boolean canRender(AssetProperties properties) {
+        return properties.getName().endsWith("txt");
+    }
+
+    @Override
+    public Parent getRoot() {
+        return new AnchorPane();
+    }
+
+    @Override
+    public String getName() {
+        return "Text";
+    }
+}
