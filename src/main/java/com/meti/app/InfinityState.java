154d6945ac2728058e8fe12458c79846e1eef276
SirMathhman <minecraftmathhman@gmail.com>
2019-06-07 13:07:16 -0400

    Add SetBasedModuleManager

--- src/main/java/com/meti/app/InfinityState.java
+++ src/main/java/com/meti/app/InfinityState.java
@@ -3,6 +3,7 @@ package com.meti.app;
 import com.meti.lib.asset.manage.SetBasedAssetManager;
 import com.meti.lib.collect.SetBasedState;
 import com.meti.lib.javafx.ListBasedStageManager;
+import com.meti.lib.module.SetBasedModuleManager;
 
 /**
  * @author SirMathhman
@@ -13,5 +14,6 @@ public final class InfinityState extends SetBasedState {
     public InfinityState() {
         add(new ListBasedStageManager());
         add(new SetBasedAssetManager());
+        add(new SetBasedModuleManager());
     }
 }
